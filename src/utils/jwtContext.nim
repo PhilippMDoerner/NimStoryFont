51b36625683a400b0bce21b390998a66671b8ff3
PhilippMDoerner <philipp3000doerner@web.de>
2022-01-15 23:07:52 +0100

    Removed unnecessary code

--- src/utils/jwtContext.nim
+++ src/utils/jwtContext.nim
@@ -1,17 +1,8 @@
 import prologue
 import ../applications/authentication/myJwt
-import options
-import strutils
-import ../applicationSettings
-import jwt
 
 type JWTContext* = ref object of Context
     tokenData*: TokenData
-
-proc getAccessToken(ctx: Context): Option[JWT] =
-    let headerValue: string = ctx.request.getHeader(AUTHORIZATION_HEADER)[0]
-    let jwtString: string = headerValue.split(' ')[1]
-    result = parseJWT(jwtString)
 
 method extend(ctx: JWTContext) =    
     ctx.tokenData = newTokenData()
