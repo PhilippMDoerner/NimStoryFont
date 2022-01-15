01a48714c71572ab6ed8517a1a92d4a97908baa6
PhilippMDoerner <philipp3000doerner@web.de>
2022-01-15 23:08:07 +0100

    Outsourced repetitive code from campaign middleware

--- src/middleware/campaignAccessMiddleware.nim
+++ src/middleware/campaignAccessMiddleware.nim
@@ -1,8 +1,7 @@
 import prologue
 import ../utils/jwtContext
 import ../utils/errorResponses
-import ../applicationSettings
-import std/[strutils, options, json]
+import std/[strutils]
 import ../applications/authentication/[authenticationModels, myJwt]
 
 type InvalidMiddlewareUseDefect* = object of Defect
@@ -30,18 +29,32 @@ proc hasCampaignAdminAccessLevel(userMemberships: Table[string, CampaignAccessLe
   let accessLevel: CampaignAccessLevel = userMemberships[campaignName]
   result = accessLevel == CampaignAccessLevel.ADMIN
 
+proc getUserCampaignMemberships(ctx: JWTContext): Table[string, CampaignAccessLevel] {.raises: InvalidMiddlewareUseDefect.} =
+  if isInitTokenData(ctx.tokenData):
+    raise newException(InvalidMiddlewareUseDefect, """Tried using a campaignAccessMiddleware 
+      on a route without using a loginMiddleware beforehand. LoginMiddleware provides the 
+      necessary data from the parsed JWT Token for authentication""")    
+  
+  result = ctx.tokenData.campaignMemberships
+
+
+proc getCampaignUrlParameter(ctx: JWTContext): string {.raises: InvalidMiddlewareUseDefect.} =
+  let campaignUrlParam = ctx.getPathParams("campaignName")
+  if campaignUrlParam == "":
+    raise newException(InvalidMiddlewareUseDefect, """Tried using campaignGuestAccessMiddleware 
+      on a route that has no campaignName url parameter. That parameter is necessary to see which
+      campaign's data is being accessed""")
+
+  result = campaignUrlParam
+
 
 proc campaignGuestAccessMiddleware*(): HandlerAsync =
   result = proc(ctx: Context) {.async.} =  
     let ctx = JWTContext(ctx)
-    let userCampaignMemberships: Table[string, CampaignAccessLevel] = ctx.tokenData.campaignMemberships
 
-    let campaignUrlParam = ctx.getPathParams("campaignName")
-    if campaignUrlParam == "":
-      raise newException(InvalidMiddlewareUseDefect, """Tried using campaignGuestAccessMiddleware 
-        on a route that has no campaignName url parameter. That parameter is necessary to see which campaign's data
-        is being accessed""")
-    
+    let userCampaignMemberships = getUserCampaignMemberships(ctx)
+    let campaignUrlParam = getCampaignUrlParameter(ctx)
+
     if not hasGuestAccessLevelOrHigher(userCampaignMemberships, campaignUrlParam):
       resp get401UnauthorizedResponse(ctx)
       return
@@ -51,13 +64,9 @@ proc campaignGuestAccessMiddleware*(): HandlerAsync =
 proc campaignMemberAccessMiddleware*(): HandlerAsync =
   result = proc(ctx: Context) {.async.} =  
     let ctx = JWTContext(ctx)
-    let userCampaignMemberships: Table[string, CampaignAccessLevel] = ctx.tokenData.campaignMemberships
 
-    let campaignUrlParam = ctx.getPathParams("campaignName")
-    if campaignUrlParam == "":
-      raise newException(InvalidMiddlewareUseDefect, """Tried using campaignGuestAccessMiddleware 
-        on a route that has no campaignName url parameter. That parameter is necessary to see which campaign's data
-        is being accessed""")
+    let userCampaignMemberships = getUserCampaignMemberships(ctx)
+    let campaignUrlParam = getCampaignUrlParameter(ctx)
     
     if not hasMemberAccessLevelOrHigher(userCampaignMemberships, campaignUrlParam):
       resp get401UnauthorizedResponse(ctx)
@@ -66,13 +75,9 @@ proc campaignMemberAccessMiddleware*(): HandlerAsync =
 proc campaignAdminAccessMiddleware*(): HandlerAsync =
   result = proc(ctx: Context) {.async.} =  
     let ctx = JWTContext(ctx)
-    let userCampaignMemberships: Table[string, CampaignAccessLevel] = ctx.tokenData.campaignMemberships
 
-    let campaignUrlParam = ctx.getPathParams("campaignName")
-    if campaignUrlParam == "":
-      raise newException(InvalidMiddlewareUseDefect, """Tried using campaignGuestAccessMiddleware 
-        on a route that has no campaignName url parameter. That parameter is necessary to see which campaign's data
-        is being accessed""")
+    let userCampaignMemberships = getUserCampaignMemberships(ctx)
+    let campaignUrlParam = getCampaignUrlParameter(ctx)
     
     if not hasCampaignAdminAccessLevel(userCampaignMemberships, campaignUrlParam):
       resp get401UnauthorizedResponse(ctx)
