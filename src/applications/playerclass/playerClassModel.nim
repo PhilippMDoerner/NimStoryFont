653edc2005d70f96e15715fd3ce3809f670e0a16
PhilippMDoerner <philipp3000doerner@web.de>
2022-01-16 12:43:55 +0100

    Moved PlayerClassModel over to using constructor

--- src/applications/playerclass/playerClassModel.nim
+++ src/applications/playerclass/playerClassModel.nim
@@ -1,50 +1,28 @@
 import norm/[model, pragmas]
 import ../../applicationSettings
+import ../../applicationConstants
 import ../character/characterModel
 import ../../utils/djangoDateTime/djangoDateTimeType
+import constructor/defaults
+
+type PlayerClass* {.defaults, tableName: PLAYERCLASS_TABLE.} = ref object of Model
+    name*: string = ""
+    update_datetime*: DjangoDateTime = djangoDateTimeType.now()
+    creation_datetime*: DjangoDateTime = djangoDateTimeType.now()
+implDefaults(PlayerClass)
+proc newModel*(T: typedesc[PlayerClass]): PlayerClass = newPlayerClass()
 
 
-type PlayerClass* {.tableName: PLAYERCLASS_TABLE.} = ref object of Model
-    name*: string
-    update_datetime*: DjangoDateTime
-    creation_datetime*: DjangoDateTime
+type PlayerClassConnectionRead* {.defaults, tableName: CHARACTER_PLAYERCLASS_TABLE.} = ref object of Model
+    player_class_id*: PlayerClass = newModel(PlayerClass)
+    character_id* {.fk: Character.}: int64 = MODEL_INIT_ID
+implDefaults(PlayerClassConnectionRead)
+proc newModel*(T: typedesc[PlayerClassConnectionRead]): PlayerClassConnectionRead = newPlayerClassConnectionRead()
 
 
-type PlayerClassConnectionRead* {.tableName: CHARACTER_PLAYERCLASS_TABLE.} = ref object of Model
-    player_class_id*: PlayerClass
-    character_id* {.fk: Character.}: int64
+type PlayerClassConnection* {.defaults, tableName: CHARACTER_PLAYERCLASS_TABLE.} = ref object of Model
+    character_id*: int64 = MODEL_INIT_ID
+    player_class_id*: int64 = MODEL_INIT_ID
+implDefaults(PlayerClassConnection)
+proc newModel*(T: typedesc[PlayerClassConnection]): PlayerClassConnection = newPlayerClassConnection()
 
-
-type PlayerClassConnection* {.tableName: CHARACTER_PLAYERCLASS_TABLE.} = ref object of Model
-    character_id*: int64
-    player_class_id*: int64
-
-
-proc newPlayerClass(
-    name = "", 
-    update_datetime = djangoDateTimeType.now(), 
-    creation_datetime = djangoDateTimeType.now()
-): PlayerClass =
-    result = PlayerClass(
-        name: name,
-        update_datetime: update_datetime,
-        creation_datetime: creation_datetime
-    )
-
-
-proc newPlayerClassConnection(character_id = -1, player_class_id = -1): PlayerClassConnection =
-    result = PlayerClassConnection(character_id: character_id, player_class_id: player_class_id)
-
-
-proc newPlayerClassConnectionRead(player_class_id = newPlayerClass()): PlayerClassConnectionRead =
-    result = PlayerClassConnectionRead(player_class_id: player_class_id)
-
-
-proc newModel*(T: typedesc[PlayerClassConnection]): PlayerClassConnection =
-    result = newPlayerClassConnection()
-
-proc newModel*(T: typedesc[PlayerClass]): PlayerClass =
-    result = newPlayerClass()
-
-proc newModel*(T: typedesc[PlayerClassConnectionRead]): PlayerClassConnectionRead =
-    result = newPlayerClassConnectionRead()
