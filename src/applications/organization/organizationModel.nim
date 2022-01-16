ee97291e590143635fb4c7174859892b5aa79dca
PhilippMDoerner <philipp3000doerner@web.de>
2022-01-16 12:40:52 +0100

    Moved over to useing default model ids

--- src/applications/organization/organizationModel.nim
+++ src/applications/organization/organizationModel.nim
@@ -1,5 +1,6 @@
 import norm/[model, pragmas]
 import ../../applicationSettings
+import ../../applicationConstants
 import ../../utils/djangoDateTime/djangoDateTimeType
 import std/options
 import ../campaign/campaignModel
@@ -8,7 +9,7 @@ import constructor/defaults
 
 type Organization* {.defaults, tableName: ORGANIZATION_TABLE.} = ref object of Model
     name: string = ""
-    campaign_id*: int64 = -1
+    campaign_id*: int64 = MODEL_INIT_ID
     update_datetime*: DjangoDateTime = djangoDateTimeType.now()
     creation_datetime*: DjangoDateTime = djangoDateTimeType.now()
     description: Option[string] = none(string)
@@ -22,7 +23,7 @@ proc newTableModel*(T: typedesc[Organization]): Organization = newOrganization()
 
 type OrganizationOverview* {.defaults, tableName: ORGANIZATION_TABLE.} = ref object of Model
     name: string = ""
-    campaign_id*: int64 = -1
+    campaign_id*: int64 = MODEL_INIT_ID
 
 implDefaults(OrganizationOverview)
 proc newModel*(T: typedesc[OrganizationOverview]): OrganizationOverview = newOrganizationOverview()
