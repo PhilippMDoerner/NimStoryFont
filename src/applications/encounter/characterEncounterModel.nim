2c4f5f34d2ba52b9fe08679577a28966e9640470
PhilippMDoerner <philipp3000doerner@web.de>
2022-01-16 12:51:20 +0100

    Moved over encounters to constructor

--- src/applications/encounter/characterEncounterModel.nim
+++ src/applications/encounter/characterEncounterModel.nim
@@ -1,34 +1,25 @@
 import norm/[model, pragmas]
 import encounterModel
 import ../../applicationSettings
+import ../../applicationConstants
 import ../character/characterModel
+import constructor/defaults
 
-type CharacterEncounterConnection* {.tableName: ENCOUNTER_CHARACTER_TABLE.} = ref object of Model
-    character_id*: int64
-    encounter_id*: int64
+type CharacterEncounterConnection* {.defaults, tableName: ENCOUNTER_CHARACTER_TABLE.} = ref object of Model
+    character_id*: int64 = MODEL_INIT_ID
+    encounter_id*: int64 = MODEL_INIT_ID
 
-proc newCharacterEncounterConnection(character_id = -1, encounter_id = -1): CharacterEncounterConnection =
-    result = CharacterEncounterConnection(
-        character_id: character_id,
-        encounter_id: encounter_id
-    )
-
+implDefaults(CharacterEncounterConnection)
 proc newModel*(T: typedesc[CharacterEncounterConnection]): CharacterEncounterConnection =
     result = newCharacterEncounterConnection()
-
-
 proc newTableModel*(T: typedesc[CharacterEncounterConnection]): CharacterEncounterConnection =
     result = newCharacterEncounterConnection()
 
 
-type CharacterEncounterRead*  {.tableName: ENCOUNTER_CHARACTER_TABLE.} = ref object of Model
-    encounter_id*: EncounterRead
-    character_id* {.fk: Character.}: int64
+type CharacterEncounterRead*  {.defaults, tableName: ENCOUNTER_CHARACTER_TABLE.} = ref object of Model
+    encounter_id*: EncounterRead = newModel(EncounterRead)
+    character_id* {.fk: Character.}: int64 = MODEL_INIT_ID
 
-
-proc newCharacterEncounterRead(encounter_id = newModel(EncounterRead)): CharacterEncounterRead =
-    result = CharacterEncounterRead(encounter_id: encounter_id)
-
-
+implDefaults(CharacterEncounterRead)
 proc newModel*(T: typedesc[CharacterEncounterRead]): CharacterEncounterRead =
     result = newCharacterEncounterRead()
--- src/applications/encounter/encounterModel.nim
+++ src/applications/encounter/encounterModel.nim
@@ -1,109 +1,62 @@
 import norm/[model, pragmas]
 import ../../applicationSettings
+import ../../applicationConstants
 import ../../utils/djangoDateTime/djangoDateTimeType
 import std/options
+import ../location/locationModel
+import constructor/defaults
 
 
-type Encounter* {.tableName: ENCOUNTER_TABLE.} = ref object of Model
-    description*: Option[string]
-    title*: Option[string]
-    diaryentry_id*: int64
-    location_id*: Option[int64]
-    order_index*: Option[int]
-    update_datetime*: DjangoDateTime
-    creation_datetime*: DjangoDateTime
+type Encounter* {.defaults, tableName: ENCOUNTER_TABLE.} = ref object of Model
+    description*: Option[string] = none(string)
+    title*: Option[string] = none(string)
+    diaryentry_id*: int64 = MODEL_INIT_ID
+    location_id* {.fk: Location.} : Option[int64] = none(int64)
+    order_index*: Option[int] = none(int)
+    update_datetime*: DjangoDateTime = djangoDateTimeType.now()
+    creation_datetime*: DjangoDateTime = djangoDateTimeType.now()
+implDefaults(Encounter)
+
+proc newModel*(T: typedesc[Encounter]): Encounter = newEncounter()
+proc newTableModel*(T: typedesc[Encounter]): Encounter = newEncounter()
 
 
-type EncounterParentLocation* {.tableName: LOCATION_TABLE.} = ref object of Model
+type EncounterParentLocation* {.defaults, tableName: LOCATION_TABLE.} = ref object of Model
     ##[HELPER MODEL*: The parent location of a location, that an encounter happened 
     in]##
-    name*: string
+    name*: string = ""
+implDefaults(EncounterParentLocation)
 
-
-type EncounterLocation* {.tableName: LOCATION_TABLE.} = ref object of Model
+type EncounterLocation* {.defaults, tableName: LOCATION_TABLE.} = ref object of Model
     ##[HELPER MODEL*: The location an encounter happened in]##
-    name*: string
-    parent_location_id*: Option[EncounterParentLocation]
+    name*: string = ""
+    parent_location_id*: Option[EncounterParentLocation] = some(newEncounterParentLocation())
+implDefaults(EncounterLocation)
 
 
-type EncounterSession* {.tableName: SESSION_TABLE.} = ref object of Model
-    session_number*: int
-    is_main_session*: bool
+type EncounterSession* {.defaults, tableName: SESSION_TABLE.} = ref object of Model
+    session_number*: int = -1
+    is_main_session*: bool = true
+implDefaults(EncounterSession)
 
 
-type EncounterDiaryentry* {.tableName: DIARYENTRY_TABLE.} = ref object of Model
-    session_id*: EncounterSession
+type EncounterDiaryentry* {.defaults, tableName: DIARYENTRY_TABLE.} = ref object of Model
+    session_id*: EncounterSession = newEncounterSession()
+implDefaults(EncounterDiaryentry)
 
 
-type EncounterRead* {.tableName: ENCOUNTER_TABLE.} = ref object of Model
-    description*: Option[string]
-    title*: Option[string]
-    diaryentry_id*: EncounterDiaryentry
-    location_id*: Option[EncounterLocation]
-    order_index*: int
-    update_datetime*: DjangoDateTime
-    creation_datetime*: DjangoDateTime
+type EncounterRead* {.defaults, tableName: ENCOUNTER_TABLE.} = ref object of Model
+    description*: Option[string] = none(string)
+    title*: Option[string] = none(string)
+    diaryentry_id*: EncounterDiaryentry = newEncounterDiaryentry()
+    location_id*: Option[EncounterLocation] = some(newEncounterLocation())
+    order_index*: Option[int] = none(int)
+    update_datetime*: DjangoDateTime = djangoDateTimeType.now()
+    creation_datetime*: DjangoDateTime = djangoDateTimeType.now()
+implDefaults(EncounterRead)
 
+proc newModel*(T: typedesc[EncounterRead]): EncounterRead = newEncounterRead()
 
-
-proc newEncounterSession(session_number = -1, is_main_session = true): EncounterSession =
-    result = EncounterSession(session_number: session_number, is_main_session: is_main_session)
-
-
-proc newEncounterDiaryEntry(session_id = newEncounterSession()): EncounterDiaryentry = 
-    result = EncounterDiaryentry(session_id: session_id)
-
-
-proc newEncounter(
-    description = none(string),
-    title = none(string),
-    diaryentry_id = -1,
-    location_id = none(int64),
-    order_index = none(int),
-    update_datetime = djangoDateTimeType.now(),
-    creation_datetime = djangoDateTimeType.now()
-): Encounter =
-    result = Encounter(
-        description: description,
-        title: title,
-        diaryentry_id: diaryentry_id,
-        location_id: location_id,
-        order_index: order_index,
-        update_datetime: update_datetime,
-        creation_datetime: creation_datetime
-    )
-
-
-proc newEncounterRead(
-    description = none(string),
-    title = none(string),
-    diaryentry_id = newEncounterDiaryEntry(),
-    location_id = none(EncounterLocation),
-    order_index = -1,
-    update_datetime = djangoDateTimeType.now(),
-    creation_datetime = djangoDateTimeType.now()
-): EncounterRead =
-    result = EncounterRead(
-        description: description,
-        title: title,
-        diaryentry_id: diaryentry_id,
-        location_id: location_id,
-        order_index: order_index,
-        update_datetime: update_datetime,
-        creation_datetime: creation_datetime
-    )
-
-
-proc newModel*(T: typedesc[EncounterRead]): EncounterRead =
-    result = newEncounterRead()
-
-
-proc newModel*(T: typedesc[Encounter]): Encounter =
-    result = newEncounter()
-
-
-proc newTableModel*(T: typedesc[Encounter]): Encounter =
-    result = newEncounter()
 
 
 
