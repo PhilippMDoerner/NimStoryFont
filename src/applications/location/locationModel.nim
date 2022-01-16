f2a18574bbeef26f40068615ab01f23615ea6659
PhilippMDoerner <philipp3000doerner@web.de>
2022-01-16 12:40:34 +0100

    Moved Location over to use constructor

--- src/applications/location/locationModel.nim
+++ src/applications/location/locationModel.nim
@@ -1,36 +1,21 @@
 import norm/[model, pragmas]
 import ../campaign/campaignModel
 import ../../applicationSettings
+import ../../applicationConstants
 import ../../utils/djangoDateTime/djangoDateTimeType
 import std/options
-
+import constructor/defaults
 
 type 
-    Location* = ref object of Model
-        name*: string
-        description*: Option[string]
-        creation_datetime*: DjangoDateTime
-        update_datetime*: DjangoDateTime
-        parent_location_id* {.fk: Location.}: Option[int64]
-        campaign_id* {.fk: Campaign.}: int64 # The id of the campaign that this character occurred in
+    Location* {.defaults, tableName: LOCATION_TABLE.}= ref object of Model
+        name*: string = ""
+        description*: Option[string] = none(string)
+        creation_datetime*: DjangoDateTime = djangoDateTimeType.now()
+        update_datetime*: DjangoDateTime = djangoDateTimeType.now()
+        parent_location_id* {.fk: Location.}: Option[int64] = none(int64)
+        campaign_id* {.fk: Campaign.}: int64 = MODEL_INIT_ID # The id of the campaign that this character occurred in
 
-
-proc newLocation(
-    name = "",
-    description: Option[string] = none(string),
-    creationDatetime: DjangoDateTime = djangoDateTimeType.now(),
-    updateDatetime: DjangoDateTime = djangoDateTimeType.now(),
-    campaign_id = -1,
-    parent_location_id = none(int64)
-): Location = 
-    result = Location(
-        name: name,
-        description: description,
-        creationDatetime: creationDatetime,
-        updateDatetime: updateDatetime,
-        campaign_id: campaign_id,
-        parent_location_id: parent_location_id
-    )
+implDefaults(Location)
 
 proc newTableModel*(T: typedesc[Location]): Location = newLocation()
 proc newModel*(T: typedesc[Location]): Location = newLocation()
