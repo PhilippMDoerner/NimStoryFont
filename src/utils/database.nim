bf869bebb6ebab555a8c0a3705f79108f6d434bd
PhilippMDoerner <philipp3000doerner@web.de>
2022-01-20 22:44:06 +0100

    Added idea of a connectionpool (does not yet work)

--- src/utils/database.nim
+++ src/utils/database.nim
@@ -1,5 +1,51 @@
 import ../applicationSettings 
 import std/[db_sqlite]
+import constructor/defaults
+import std/[times, locks]
+
 
 proc createRawDatabaseConnection*(): DbConn =
     return open(applicationSettings.database, "", "", "")
+
+
+type PoolConnection* {.defaults.} = object
+  connection: DbConn = createRawDatabaseConnection()
+  deathTime: DateTime = now() + initTimeInterval(days = 1)
+
+implDefaults(PoolConnection)
+
+type ConnectionPool* = object
+  connections: seq[PoolConnection]
+  lock: Lock
+
+var POOL: ConnectionPool
+proc isEmptyPool(): bool = POOL.connections.len() == 0
+
+proc initConnectionPool*(initialPoolSize: static int) = 
+  POOL = ConnectionPool(connections: @[])
+  initLock(POOL.lock)
+
+  for i in 1..initialPoolSize:
+    POOL.connections.add(initPoolConnection())
+
+
+proc borrowConnection*(): PoolConnection =
+  acquire(POOL.lock)
+
+  if isEmptyPool():
+    return initPoolConnection()
+
+  result = POOL.connections.pop()
+
+  release(POOL.lock)
+
+proc recycleConnection*(connection: sink PoolConnection) =
+  if connection.deathTime < now():
+    return
+  
+  acquire(POOL.lock)
+  POOL.connections.add(connection)
+  release(POOL.lock)
+
+proc destroyConnectionPool*() =
+  deinitLock(POOL.lock)
