77bcd551305c160d4c17cc20836550622c65d7df
PhilippMDoerner <philipp3000doerner@web.de>
2022-01-22 17:50:06 +0100

    Got imageModel to use constructor

--- src/applications/image/imageModel.nim
+++ src/applications/image/imageModel.nim
@@ -1,6 +1,7 @@
 import norm/[model, pragmas]
 import ../../applicationSettings
 import std/options
+import constructor/defaults
 
 type ImageType* = enum
     CHARACTERTYPE = "character", 
@@ -16,38 +17,14 @@ type ##[Exist solely to inform Image model to which table a given fk-model point
     ImageCreature {.tableName: CREATURE_TABLE.} = ref object of Model
     ImageOrganization {.tableName: ORGANIZATION_TABLE.} = ref object of Model
 
-type Image* {.tableName: IMAGE_TABLE.} = ref object of Model
-    image*: string
-    name*: Option[string]
-    character_article_id* {.fk: ImageCharacter.}: Option[int64]
-    creature_article_id* {.fk: ImageCreature.}: Option[int64]
-    item_article_id* {.fk: ImageItem.} : Option[int64]
-    location_article_id* {.fk: ImageLocation.}: Option[int64]
-    organization_article_id* {.fk: ImageOrganization.}: Option[int64]
+type Image* {.defaults, tableName: IMAGE_TABLE.} = ref object of Model
+    image*: string = ""
+    name*: Option[string] = some("")
+    character_article_id* {.fk: ImageCharacter.}: Option[int64] = none(int64)
+    creature_article_id* {.fk: ImageCreature.}: Option[int64] = none(int64)
+    item_article_id* {.fk: ImageItem.} : Option[int64] = none(int64)
+    location_article_id* {.fk: ImageLocation.}: Option[int64] = none(int64)
+    organization_article_id* {.fk: ImageOrganization.}: Option[int64] = none(int64)
 
-
-proc newImage*(
-    image = "",
-    name = none(string),
-    character_article_id = none(int64),
-    creature_article_id = none(int64),
-    item_article_id = none(int64),
-    location_article_id = none(int64),
-    organization_article_id = none(int64),
-): Image = 
-    result = Image(
-        image: image,
-        name: name,
-        character_article_id: character_article_id,
-        creature_article_id: creature_article_id,
-        item_article_id: item_article_id,
-        location_article_id: location_article_id,
-        organization_article_id: organization_article_id
-    )
-
-
-
-proc newModel*(T: typedesc[Image]): Image =
-    ##[Enables generic instantiation of the model. This enable the use of 
-    generic methods that only read the database in genericArticleRepository ]##
-    result = newImage()
+implDefaults(Image)
+proc newModel*(T: typedesc[Image]): Image = newImage()
