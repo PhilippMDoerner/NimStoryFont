e7aec3ebcf925edcc7eadb15e130bedac595edd3
PhilippMDoerner <philipp3000doerner@web.de>
2022-01-22 17:54:35 +0100

    Added constants for media directory settings

--- src/applicationSettings.nim
+++ src/applicationSettings.nim
@@ -5,14 +5,17 @@ import times
 const database* = "db.sqlite3"
 const CONNECTION_POOL_SIZE*: int = 20
 
+const BASE_DIR* = "/home/isofruit/dev/nimstoryfont"
+const MEDIA_ROOT* = BASE_DIR & "/media"
+const MEDIA_URL* = "/media/"
+
 const DEBUG*: bool = true
 const ACCESS_TOKEN_LIFETIME*: TimeInterval = hours(1) # 1h in seconds
 const REFRESH_TOKEN_LIFETIME*: TimeInterval = days(100) # 100 days in seconds
 
 #Prologue Settings
 let coreSettings*: Settings = loadSettings("./settings.json")
-
-const SECRET_KEY*: string = "MyBeautifulSecretKey"
+const SECRET_KEY*: string = "MyBeautifulSecretKey" #TODO: write this so this can be loaded in on startup and not compiled into the binary
 const AUTHORIZATION_HEADER*: string = "Authorization"
 
 
