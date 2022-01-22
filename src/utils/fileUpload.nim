347e2e27403fbfa12a1e4f0d65709f44cbae92de
PhilippMDoerner <philipp3000doerner@web.de>
2022-01-22 17:51:56 +0100

    #24 added utility function for image upload

---
+++ src/utils/fileUpload.nim
@@ -0,0 +1,28 @@
+import prologue
+import ../applicationSettings
+import std/[os, random]
+
+type FileNotFoundError* = object of IOError
+type FileAlreadyExists* = object of IOError
+
+
+proc randomString(length: int): string =
+    for _ in 0..length:
+        add(result, char(rand(int('A') .. int('z'))))
+
+
+proc uploadArticleImage*(file: var UpLoadFile): string =
+  let articleImageDirectory = MEDIA_ROOT & "/article_images"
+  if not dirExists(articleImageDirectory):
+    raise newException(FileNotFoundError, "The article image directory '" & articleImageDirectory & "' does not exist")
+  
+  var filePath = articleImageDirectory & '/' & file.filename
+  if fileExists(filePath):
+    let (directory, name, extension) = file.filename.splitFile()
+    let newFileName = name & '_' & randomString(10) & '.' & extension
+    file.filename = newFileName
+    filePath = articleImageDirectory & '/' & newFileName
+
+  file.save(articleImageDirectory)
+  
+  result = filePath
