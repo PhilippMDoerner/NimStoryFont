4dd4754c75202560abeb7d5fbc0a5dab0f449059
PhilippMDoerner <philipp3000doerner@web.de>
2022-01-29 13:09:29 +0100

    made private fields public

--- src/applications/session/sessionModel.nim
+++ src/applications/session/sessionModel.nim
@@ -9,9 +9,9 @@ import ../campaign/campaignModel
 type Session* {.defaults, tableName: SESSION_TABLE} = ref object of Model
   session_number: int = -1
   session_date*: DjangoDateTime = djangoDateTimeType.now()
-  is_main_session: bool = true
-  end_day: Option[int] = some(-1)
-  start_day: Option[int] = some(-1)
+  is_main_session*: bool = true
+  end_day*: Option[int] = some(-1)
+  start_day*: Option[int] = some(-1)
   creation_datetime*: DjangoDateTime = djangoDateTimeType.now()
   update_datetime*: DjangoDateTime = djangoDateTimeType.now()
   campaign_id* {.fk: Campaign.}: int64 = MODEL_INIT_ID
