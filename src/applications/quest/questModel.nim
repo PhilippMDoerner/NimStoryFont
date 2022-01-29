86dba06cac09ab11b231ee927acfbae8523451d3
PhilippMDoerner <philipp3000doerner@web.de>
2022-01-29 12:46:11 +0100

    #27 made fields public that were accidentally private

--- src/applications/quest/questModel.nim
+++ src/applications/quest/questModel.nim
@@ -9,13 +9,13 @@ import ../character/characterModel
 import ../session/sessionModel
 
 type Quest* {.defaults, tableName: QUEST_TABLE} = ref object of Model
-  name: string = ""
-  status: string = ""
-  taker_id {.fk: Character.} : Option[int64] = some(MODEL_INIT_ID)
-  abstract: Option[string] = some("")
-  description: Option[string] = some("")
-  start_session_id {.fk: Session.}: int64 = MODEL_INIT_ID
-  end_session_id {.fk: Session.}: Option[int64] = some(MODEL_INIT_ID)
+  name*: string = ""
+  status*: string = ""
+  taker_id* {.fk: Character.}: Option[int64] = some(MODEL_INIT_ID)
+  abstract*: Option[string] = some("")
+  description*: Option[string] = some("")
+  start_session_id* {.fk: Session.}: int64 = MODEL_INIT_ID
+  end_session_id* {.fk: Session.}: Option[int64] = some(MODEL_INIT_ID)
   creation_datetime*: DjangoDateTime = djangoDateTimeType.now()
   update_datetime*: DjangoDateTime = djangoDateTimeType.now()
   campaign_id* {.fk: Campaign.}: int64 = MODEL_INIT_ID
