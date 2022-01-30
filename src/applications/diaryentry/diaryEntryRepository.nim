ded0793fd32a0a043aae5208c4184c7f79d8a4cd
PhilippMDoerner <philipp3000doerner@web.de>
2022-01-30 10:22:48 +0100

    #2 added helper proc to query encounters

--- src/applications/diaryentry/diaryEntryRepository.nim
+++ src/applications/diaryentry/diaryEntryRepository.nim
@@ -1,7 +1,21 @@
 import diaryEntryModel
+import ../encounter/encounterModel
 import ../base_generics/genericArticleRepository
+import ../../applicationSettings
+import norm/model
+import tinypool
+import std/strformat
 
 export diaryEntryModel
 
 proc getDiaryEntryById*(diaryentryId: int64): DiaryEntry =
-    result = getEntryById[DiaryEntry](diaryentryId)
+  result = getEntryById[DiaryEntry](diaryentryId)
+
+proc getDiaryEntryEncounters(diaryentryId: int64): seq[Encounter] =
+    var encounters: seq[Encounter] = @[newModel(Encounter)]
+    let sqlCondition: string = fmt "{ENCOUNTER_TABLE}.diaryentry_id = ?"
+
+    withDbConn(connection):
+        connection.select(encounters, sqlCondition, diaryentryId)
+
+    result = encounters
