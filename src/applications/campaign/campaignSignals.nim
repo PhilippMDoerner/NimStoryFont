199053648bf25e6d6a554a47b9af6c9a0973f2fe
PhilippMDoerner <philipp3000doerner@web.de>
2022-02-05 20:34:04 +0100

    Added further campaign creation signals

--- src/applications/campaign/campaignSignals.nim
+++ src/applications/campaign/campaignSignals.nim
@@ -1,22 +1,10 @@
 import ../genericArticleRepository
 import ../django/djangoModels
-import ../authentication/authenticationModels
+import ../authentication/[authenticationRepository, authenticationConstants, authenticationModels]
 import campaignUtils
 import campaignModel
 import ../core/signalSystem
-
-proc createCampaignGroup(connection: DbConn, groupName: string) =
-  var group: Group = Group(name: groupName)
-  discard createEntryInTransaction(connection, group)
-
-proc createGuestGroup(connection: DbConn, instance: Campaign) = createCampaignGroup(connection, instance.guestGroupName)
-proc createMemberGroup(connection: DbConn, instance: Campaign) = createCampaignGroup(connection, instance.memberGroupName)
-proc createAdminGroup(connection: DbConn, instance: Campaign) = createCampaignGroup(connection, instance.adminGroupName)
-
-connect(SignalType.stPostCreate, Campaign, createGuestGroup)
-connect(SignalType.stPostCreate, Campaign, createMemberGroup)
-connect(SignalType.stPostCreate, Campaign, createAdminGroup)
-
+import std/sequtils
 
 proc createCampaignPermission*(connection: DbConn, permissionName: string, codeName: string) =
   var campaignContentType = getEntryByField[DjangoContentType, string]("model", "campaign")
@@ -36,6 +24,49 @@ proc createMemberPermission*(connection: DbConn, instance: Campaign) =
 proc createAdminPermission*(connection: DbConn, instance: Campaign) =
   createCampaignPermission(connection, instance.adminPermissionName, instance.adminPermissionCodename)
 
-connect(SignalType.stPostCreate, Campaign, createGuestPermission)
-connect(SignalType.stPostCreate, Campaign, createMemberPermission)
-connect(SignalType.stPostCreate, Campaign, createAdminPermission)
+proc createCampaignGroup(connection: DbConn, groupName: string): Group =
+  var group: Group = Group(name: groupName)
+  result = createEntryInTransaction(connection, group)
+
+proc addCampaignPermissions(connection: DbConn, group: Group, permissionCodeNames: seq[string]) =
+  let permissions: seq[Permission] = getPermissions(connection, permissionCodeNames)
+  for permission in permissions:
+    var groupPermission = GroupPermission(group_id: group.id, permission_id: permission.id)
+    discard createEntryInTransaction(connection, groupPermission)
+
+proc createGuestGroup(connection: DbConn, instance: Campaign) = 
+  ## Creates the guest group for a campaign. Also grants it all the permissions
+  ## that this group would have on the given campaign in addition to the guest Permission
+  let guestGroup: Group = createCampaignGroup(connection, instance.guestGroupName)
+  
+  let guestPermissionCodenames: seq[string] = concat(CAMPAIGN_GUEST_PERMISSIONS, @[instance.guestPermissionCodename])
+  addCampaignPermissions(connection, guestGroup, guestPermissionCodenames)
+
+proc createMemberGroup(connection: DbConn, instance: Campaign) =
+  ## Creates the member group for a campaign. Also grants it all the permissions
+  ## that this group would have on the given campaign in addition to the member Permission
+  let memberGroup: Group = createCampaignGroup(connection, instance.memberGroupName)
+  
+  let memberPermissionCodenames: seq[string] = concat(CAMPAIGN_MEMBER_PERMISSIONS, @[instance.memberPermissionCodename])
+  addCampaignPermissions(connection, memberGroup, memberPermissionCodenames)
+
+proc createAdminGroup(connection: DbConn, instance: Campaign) =
+  ## Creates the admin group for a campaign. Also grants it all the permissions
+  ## that this group would have on the given campaign in addition to the admin Permission
+  let adminGroup: Group = createCampaignGroup(connection, instance.adminGroupName)
+  
+  let adminPermissionCodenames: seq[string] = concat(CAMPAIGN_ADMIN_PERMISSIONS, @[instance.adminPermissionCodename])
+  addCampaignPermissions(connection, adminGroup, adminPermissionCodenames)
+
+
+
+proc createCampaignGroupsAndPermissions(connection: DbConn, modelInstance: Campaign) =
+  createGuestPermission(connection, modelInstance)
+  createMemberPermission(connection, modelInstance)
+  createAdminPermission(connection, modelInstance)
+
+  createGuestGroup(connection, modelInstance)
+  createMemberGroup(connection, modelInstance)
+  createAdminGroup(connection, modelInstance)
+  
+connect(SignalType.stPostCreate, Campaign, createCampaignGroupsAndPermissions)
