78039b008eb9da244f744a9c589e5a41a420af7d
PhilippMDoerner <philipp3000doerner@web.de>
2022-02-05 17:05:29 +0100

    Added a bunch of campaign utils for the campaign model #20

--- src/applications/campaign/campaignUtils.nim
+++ src/applications/campaign/campaignUtils.nim
@@ -1,4 +1,17 @@
 import campaignModel
+import std/strformat
 
 proc `$`*(model: CampaignOverview | MinimumCampaignOverview | CampaignRead | Campaign): string = 
-    result = model.name
+  result = model.name
+
+proc guestPermissionName*(instance: Campaign): string = fmt "Is {instance.name} guest"
+proc memberPermissionName*(instance: Campaign): string = fmt "Is {instance.name} campaign member"
+proc adminPermissionName*(instance: Campaign): string = fmt "Is {instance.name} admin"  
+
+proc guestPermissionCodename*(instance: Campaign): string = fmt "{instance.name}_read_campaign_permission"
+proc memberPermissionCodename*(instance: Campaign): string = "add_change_delete_{instance.name}_campaign_permission" 
+proc adminPermissionCodename*(instance: Campaign): string = "admin_{instance.name}_campaign_permission"
+
+proc guestGroupName*(instance: Campaign): string = "{instance.name}_guest_campaign_group"
+proc memberGroupName*(instance: Campaign): string = "{instance.name}_campaign_group" 
+proc adminGroupName*(instance: Campaign): string = "{instance.name}_campaign_admin_group" 
