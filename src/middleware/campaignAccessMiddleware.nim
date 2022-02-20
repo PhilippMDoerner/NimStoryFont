8189ab67db7a438a50ff3c8af611ba1a832ed049
PhilippMDoerner <philipp3000doerner@web.de>
2022-02-20 20:13:38 +0100

    Added "Debug" to campaignAccessMiddleware

--- src/middleware/campaignAccessMiddleware.nim
+++ src/middleware/campaignAccessMiddleware.nim
@@ -3,6 +3,7 @@ import ../utils/jwtContext
 import ../utils/errorResponses
 import std/[strutils]
 import ../applications/authentication/[authenticationModels, myJwt]
+import ../applicationSettings
 
 type InvalidMiddlewareUseDefect* = object of Defect
 
@@ -29,19 +30,18 @@ proc hasCampaignAdminAccessLevel(userMemberships: Table[string, CampaignAccessLe
   let accessLevel: CampaignAccessLevel = userMemberships[campaignName]
   result = accessLevel == CampaignAccessLevel.ADMIN
 
-proc getUserCampaignMemberships(ctx: JWTContext): Table[string, CampaignAccessLevel] {.raises: InvalidMiddlewareUseDefect.} =
-  if isInitTokenData(ctx.tokenData):
-    raise newException(InvalidMiddlewareUseDefect, """Tried using a campaignAccessMiddleware 
+proc getUserCampaignMemberships(ctx: JWTContext): Table[string, CampaignAccessLevel] =
+  assert(isInitTokenData(ctx.tokenData), """Tried using a campaignAccessMiddleware 
       on a route without using a loginMiddleware beforehand. LoginMiddleware provides the 
-      necessary data from the parsed JWT Token for authentication""")    
-  
+      necessary data from the parsed JWT Token for authentication""")
+
   result = ctx.tokenData.campaignMemberships
 
 
-proc getCampaignUrlParameter(ctx: JWTContext): string {.raises: InvalidMiddlewareUseDefect.} =
+proc getCampaignUrlParameter(ctx: JWTContext): string =
   let campaignUrlParam = ctx.getPathParams("campaignName")
-  if campaignUrlParam == "":
-    raise newException(InvalidMiddlewareUseDefect, """Tried using campaignGuestAccessMiddleware 
+  
+  assert(campaignUrlParam == "", """Tried using campaignGuestAccessMiddleware 
       on a route that has no campaignName url parameter. That parameter is necessary to see which
       campaign's data is being accessed""")
 
@@ -50,39 +50,42 @@ proc getCampaignUrlParameter(ctx: JWTContext): string {.raises: InvalidMiddlewar
 
 proc campaignGuestAccessMiddleware*(): HandlerAsync =
   result = proc(ctx: Context) {.async.} =  
-    let ctx = JWTContext(ctx)
+    when not DEBUG:
+      let ctx = JWTContext(ctx)
 
-    let userCampaignMemberships = getUserCampaignMemberships(ctx)
-    let campaignUrlParam = getCampaignUrlParameter(ctx)
+      let userCampaignMemberships = getUserCampaignMemberships(ctx)
+      let campaignUrlParam = getCampaignUrlParameter(ctx)
 
-    if not hasGuestAccessLevelOrHigher(userCampaignMemberships, campaignUrlParam):
-      resp get401UnauthorizedResponse(ctx)
-      return
+      if not hasGuestAccessLevelOrHigher(userCampaignMemberships, campaignUrlParam):
+        resp get401UnauthorizedResponse(ctx)
+        return
 
     await switch(ctx)
 
 proc campaignMemberAccessMiddleware*(): HandlerAsync =
   result = proc(ctx: Context) {.async.} =  
-    let ctx = JWTContext(ctx)
+    when not DEBUG:
+      let ctx = JWTContext(ctx)
 
-    let userCampaignMemberships = getUserCampaignMemberships(ctx)
-    let campaignUrlParam = getCampaignUrlParameter(ctx)
-    
-    if not hasMemberAccessLevelOrHigher(userCampaignMemberships, campaignUrlParam):
-      resp get401UnauthorizedResponse(ctx)
-      return
+      let userCampaignMemberships = getUserCampaignMemberships(ctx)
+      let campaignUrlParam = getCampaignUrlParameter(ctx)
+      
+      if not hasMemberAccessLevelOrHigher(userCampaignMemberships, campaignUrlParam):
+        resp get401UnauthorizedResponse(ctx)
+        return
 
     await switch(ctx)
 
 proc campaignAdminAccessMiddleware*(): HandlerAsync =
   result = proc(ctx: Context) {.async.} =  
-    let ctx = JWTContext(ctx)
+    when not DEBUG:
+      let ctx = JWTContext(ctx)
 
-    let userCampaignMemberships = getUserCampaignMemberships(ctx)
-    let campaignUrlParam = getCampaignUrlParameter(ctx)
-    
-    if not hasCampaignAdminAccessLevel(userCampaignMemberships, campaignUrlParam):
-      resp get401UnauthorizedResponse(ctx)
-      return
+      let userCampaignMemberships = getUserCampaignMemberships(ctx)
+      let campaignUrlParam = getCampaignUrlParameter(ctx)
+      
+      if not hasCampaignAdminAccessLevel(userCampaignMemberships, campaignUrlParam):
+        resp get401UnauthorizedResponse(ctx)
+        return
 
     await switch(ctx)
