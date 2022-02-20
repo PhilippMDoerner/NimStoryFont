b3653d4648121df6165b24ab6e8103c8c9968840
PhilippMDoerner <philipp3000doerner@web.de>
2022-02-20 20:14:25 +0100

    Added comments

--- src/nimstoryfont.nim
+++ src/nimstoryfont.nim
@@ -3,9 +3,10 @@ import applicationSettings
 import utils/jwtContext
 import logging
 import tinypool
-import applications/allSignals
+import applications/allSignals #Necessary so that signals get loaded
 import routes
 
+# TODO: Check out if there is a performance difference on using --gc:orc
 proc main() =
     initConnectionPool(applicationSettings.database, CONNECTION_POOL_SIZE)
 
