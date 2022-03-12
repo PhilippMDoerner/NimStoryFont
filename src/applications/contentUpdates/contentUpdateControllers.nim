cc27cff8d1eaf334f5c4db33d8e9046fac22abe1
PhilippMDoerner <philippmdoerner@web.de>
2022-03-12 13:49:34 +0100

    #19 Added first draft for "recently Updated" endpoints

---
+++ src/applications/contentUpdates/contentUpdateControllers.nim
@@ -0,0 +1,19 @@
+import contentUpdateModel
+import contentUpdateService
+import prologue
+import jsony
+import ../controllerTemplates
+import ../../utils/[jwtContext, customResponses, errorResponses]
+import ../allUrlParams
+import std/strutils
+
+proc getRecentlyUpdatedArticles*(ctx: Context) {.async.} = 
+    let ctx = JWTContext(ctx)
+    
+    let campaignName: string = ctx.getPathParams(CAMPAIGN_NAME_PARAM)
+    let pageNumber: int = ctx.getPathParams(PAGE_NUMBER_PARAM).parseInt()
+    let pageSize: int = ctx.gScope.settings.getOrDefault("pageSize").getInt()
+
+    respondBadRequestOnDbError():
+        let articles: seq[ContentUpdateSerializable] = contentUpdateService.getRecentlyUpdatedArticles(campaignName, pageNumber, pageSize)
+        resp jsonyResponse(ctx, articles)
---
+++ src/applications/contentUpdates/contentUpdateRoutes.nim
@@ -0,0 +1,20 @@
+import prologue
+import ../../middleware/[loginMiddleware, campaignAccessMiddleware]
+import contentUpdateControllers
+import std/strformat
+import ../allUrlParams
+
+proc addArticleUpdateRoutes*(app: Prologue) =
+    app.addRoute(
+        re fmt"/recentUpdates/{CAMPAIGN_NAME_PATTERN}/",
+        handler = contentUpdateControllers.getRecentlyUpdatedArticles,
+        httpMethod = HttpGet,
+        middlewares = @[loginMiddleware(), campaignGuestAccessMiddleware()]
+    )
+
+    app.addRoute(
+        re fmt"/recentUpdates/{CAMPAIGN_NAME_PATTERN}/{PAGE_NUMBER_PATTERN}",
+        handler = contentUpdateControllers.getRecentlyUpdatedArticles,
+        httpMethod = HttpGet,
+        middlewares = @[loginMiddleware(), campaignGuestAccessMiddleware()]
+    )
