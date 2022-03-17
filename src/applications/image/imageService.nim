17da154f0c30113f6f36f27d2f1b052d07ef5f38
Philipp Doerner <show>
2022-03-17 08:52:53 +0100

    #9 light image API endpoint refactoring

--- src/applications/image/imageService.nim
+++ src/applications/image/imageService.nim
@@ -35,50 +35,40 @@ proc getFormImageId(ctx: Context, imageIdFieldName: string): Option[int64] =
     return some(imageId)
 
 
-proc createImage*(ctx: JWTContext): Image =
-  var file: UpLoadFile = ctx.getUploadFile("image")
-  let mediaDirectory: string = ctx.getSettings("mediaDir").getStr()
-  let filePath: string = uploadArticleImage(file, mediaDirectory)
+proc createImage*(imageDTO: var ImageDTO): Option[Image] =
+  if imageDTO.imageFile.isNone():
+    return none(Image)
+
+  let filePath: string = uploadArticleImage(imageDTO.imageFile.get(), imageDTO.imageDirectory)
   
-  var img: Image = newModel(Image)
-  img.image = filePath
-  img.character_article_id = ctx.getFormImageId("character_article")
-  img.creature_article_id = ctx.getFormImageId("creature_article")
-  img.location_article_id = ctx.getFormImageId("location_article")
-  img.organization_article_id = ctx.getFormImageId("organization_article")
-  img.item_article_id = ctx.getFormImageId("item_article")
+  var img: Image = Image(
+    image: filePath,
+    character_article_id: imageDTO.image_character_fk,
+    creature_article_id: imageDTO.image_creature_fk,
+    location_article_id: imageDTO.image_location_fk,
+    organization_article_id: imageDTO.image_organization_fk,
+    item_article_id: imageDTO.image_item_fk,
+    name: imageDTO.imageName
+  )
 
-  result = createEntry(img)
+  result = some(createEntry(img))
 
-proc updateImage*(imageId: int64, imageDTO: var ImageDTO): Image =
-
+proc updateImageFileOrName*(imageId: int64, imageDTO: var ImageDTO): Image =
+  ## This is a special form of updating, as it is for updating individual fields
+  ## which may or may not be specified. If they are specified, edit the values
+  ## in the entry and persist them to the database. If they aren't, ignore them.
   var imageToUpdate: Image
+
+  {.cast(gcsafe).}:
+    withDbTransaction(connection):
+      imageToUpdate = connection.getEntryById(imageId, Image)
+
+      if imageDTO.imageFile.isSome():
+        let newImageFilePath: string = uploadArticleImage(imageDTO.imageFile.get(), imageDTO.imageDirectory)
+        imageToUpdate.image = newImageFilePath
+
+      if imageDTO.imageName.isSome():
+        imageToUpdate.name = imageDTO.imageName
+      
+      result = connection.updateEntryInTransaction(imageToUpdate)
   
-  withDbTransaction(connection):
-    imageToUpdate = connection.getEntryById(imageId, Image)
-
-    if imageDTO.imageFile.isSome():
-      let newImageFilePath: string = uploadArticleImage(imageDTO.imageFile.get(), imageDTO.imageDirectory)
-      imageToUpdate.image = newImageFilePath
-
-    if imageDTO.imageName.isSome():
-      imageToUpdate.name = imageDTO.imageName
-
-    if imageDTO.image_character_fk.isSome():
-      imageToUpdate.character_article_id = imageDTO.image_character_fk
-    
-    if imageDTO.image_creature_fk.isSome():
-      imageToUpdate.creature_article_id = imageDTO.image_creature_fk
-    
-    if imageDTO.image_item_fk.isSome():
-      imageToUpdate.item_article_id = imageDTO.image_item_fk
-    
-    if imageDTO.image_location_fk.isSome():
-      imageToUpdate.location_article_id = imageDTO.image_location_fk
-    
-    if imageDTO.image_organization_fk.isSome():
-      imageToUpdate.organization_article_id = imageDTO.image_organization_fk
-    
-    connection.update(imageToUpdate)
-  
-  result = imageToUpdate
