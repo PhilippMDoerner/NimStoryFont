2b9ceb7052832fc40d809ab3b363e49f0f582308
Philipp Doerner <show>
2022-03-19 11:55:35 +0100

    #1 simplified query significantly

--- src/applications/diaryentry/diaryEntryRepository.nim
+++ src/applications/diaryentry/diaryEntryRepository.nim
@@ -6,14 +6,7 @@ import ../../utils/djangoDateTime/[normConversion, djangoDateTimeType]
 
 proc getDiaryEntriesForCampaign*(campaignName: string): seq[DiaryEntryOverview] =
   var entries: seq[DiaryEntryOverview] = @[newModel(DiaryEntryOverview)]
-  let condition: string = """
-    session_id.id IN (
-      SELECT wikientries_session.id
-      FROM wikientries_session 
-      INNER JOIN wikientries_campaign ON wikientries_session.campaign_id = wikientries_campaign.id
-      WHERE wikientries_campaign.name LIKE ?
-    )
-  """
+  const condition: string = " session_id_campaign_id.name LIKE ?"
 
   withDbConn(connection):
     connection.select(entries, condition, campaignName)
