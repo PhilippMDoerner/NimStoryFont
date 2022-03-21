73a7574337a3cb4e71599e1aa83d469bc3a908fe
Philipp Doerner <show>
2022-03-21 22:37:37 +0100

    Moved over to creating controllers generically

--- src/applications/genericArticleControllers.nim
+++ src/applications/genericArticleControllers.nim
@@ -6,7 +6,7 @@ import genericArticleRepository
 import allUrlParams
 import norm/model
 
-proc deleteEntryControllerFactory*[T: Model](modelType: typedesc[T]): HandlerAsync =
+proc createEntryDeletionHandler*[T: Model](modelType: typedesc[T]): HandlerAsync =
   result = proc (ctx: Context) {.async.} =
     let ctx = JWTContext(ctx)
 
@@ -15,3 +15,14 @@ proc deleteEntryControllerFactory*[T: Model](modelType: typedesc[T]): HandlerAsy
     respondBadRequestOnDbError():
       deleteEntry(entryId, modelType)
       respDefault(Http204)
+
+proc createEntryCreationHandler*[T: Model, M: object | ref object](modelType: typedesc[T], getSerializedArticleData: proc(entryId: int64): M {.gcsafe.}): HandlerAsync =
+  result = proc (ctx: Context) {.async, gcsafe.}=
+    let ctx = JWTContext(ctx)
+
+    let jsonData: string = ctx.request.body()
+    
+    respondBadRequestOnDbError():
+        let newEntry = createArticleEntry(jsonData, modelType)
+        let serializedNewEntry = getSerializedArticleData(newEntry.id)
+        resp jsonyResponse(ctx, serializedNewEntry)
