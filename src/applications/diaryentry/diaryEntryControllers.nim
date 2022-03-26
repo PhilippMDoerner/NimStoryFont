4d2eddbb99bec11497e71f64939030dfb5c1eed9
PhilippMDoerner <philippmdoerner@web.de>
2022-03-26 07:51:37 +0100

    Removed unused controllers

--- src/applications/diaryentry/diaryEntryControllers.nim
+++ src/applications/diaryentry/diaryEntryControllers.nim
@@ -8,31 +8,3 @@ import jsony
 import ../controllerTemplates
 import ../allUrlParams
 
-
-proc getCampaignDiaryEntryOverviewView*(ctx: Context) {.async.} = 
-    let ctx = JWTContext(ctx)
-    
-    let campaignName: string = ctx.getPathParams(CAMPAIGN_NAME_PARAM)
-
-    respondBadRequestOnDbError():
-        let diaryEntries: seq[DiaryEntryOverview] = diaryEntryService.getCampaignDiaryEntryListOverview(campaignName)
-        resp jsonyResponse[seq[DiaryEntryOverview]](ctx, diaryEntries)
-
-proc createDiaryEntryView*(ctx: Context) {.async.} =
-    let ctx = JWTContext(ctx)
-
-    let jsonData: string = ctx.request.body()
-    
-    respondBadRequestOnDbError():
-        let newEntry = createDiaryEntry(jsonData)
-        resp jsonyResponse(ctx, newEntry)
-
-proc deleteDiaryEntryView*(ctx: Context) {.async.} =
-    let ctx = JWTContext(ctx)
-
-    let diaryEntryId: int64 = parseInt(ctx.getPathParams(ID_PARAM))
-
-    respondBadRequestOnDbError():
-        deleteDiaryEntry(diaryEntryId)
-        respDefault(Http204)
-
