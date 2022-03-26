6ac2534b9cedf8d60159e71b7f937f9f9ded2098
Philipp Doerner <show>
2022-03-26 18:09:25 +0100

    Added Rule API endpoints
    closes #15

--- src/applications/rules/ruleService.nim
+++ src/applications/rules/ruleService.nim
@@ -5,3 +5,12 @@ export ruleModel
 
 proc getRuleById*(ruleId: int64): Rule =
     result = getEntryById(ruleId, Rule)
+
+proc getRuleSerialization*(connection: DbConn, entry: Rule): Rule =
+    result = entry
+
+proc getCampaignRulesList*(campaignName: string): seq[Rule] =
+    result = getCampaignList(campaignName, Rule)
+
+proc getRuleByName*(campaignName: string, entryName: string): Rule =
+    result = getEntryByName(campaignName, entryName, Rule)
