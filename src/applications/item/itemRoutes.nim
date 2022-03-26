546aaf4e5ff2dae8813aa3f94d083667bcd032cc
PhilippMDoerner <philippmdoerner@web.de>
2022-03-26 07:53:02 +0100

    #3 Added generic controllers

---
+++ src/applications/item/itemRoutes.nim
@@ -0,0 +1,52 @@
+import prologue
+import ../../middleware/[loginMiddleware, campaignAccessMiddleware]
+import ../allUrlParams
+import itemControllers
+import itemModel
+import itemService
+import std/strformat
+import ../genericArticleControllers
+
+proc addItemRoutes*(app: Prologue) =
+    app.addRoute(
+        re"/item/",
+        handler = createEntryCreationHandler(Item, getSerializedItem),
+        httpMethod = HttpPost,
+        middlewares = @[loginMiddleware(), campaignMemberAccessMiddleware()]
+    )
+
+    app.addRoute(
+        re fmt"/item/{ID_PATTERN}/", 
+        handler = createEntryDeletionHandler(Item, ID_PARAM),
+        httpMethod = HttpDelete,
+        middlewares = @[loginMiddleware(), campaignMemberAccessMiddleware()]
+    )
+
+    app.addRoute(
+        re fmt"/item/{ID_PATTERN}/", 
+        handler = createEntryUpdateHandler(Item, ID_PARAM, getSerializedItem),
+        httpMethod = HttpPut,
+        middlewares = @[loginMiddleware(), campaignMemberAccessMiddleware()]
+    )
+
+    app.addRoute(
+        re fmt"/item/{ID_PATTERN}/", 
+        handler = createEntryReadByIdHandler(ID_PARAM, getItemById), 
+        httpMethod = HttpGet,
+        middlewares = @[loginMiddleware(), campaignMemberAccessMiddleware()]
+    )
+
+    app.addRoute(
+        re fmt"/item/{CAMPAIGN_NAME_PATTERN}/overview/", 
+        handler = createCampaignOverviewReadHandler(CAMPAIGN_NAME_PARAM, getCampaignItemListOverview),
+        httpMethod = HttpGet,
+        middlewares = @[loginMiddleware(), campaignMemberAccessMiddleware()]
+    )
+    
+    # app.addRoute(
+    #     re fmt"/item/{CAMPAIGN_NAME_PATTERN}/{ARTICLE_NAME_PATTERN}/", 
+    #     characterControllers.getCharacterByNameView,  
+    #     httpMethod = HttpGet,
+    #     middlewares = @[loginMiddleware(), campaignMemberAccessMiddleware()]
+    # )
+   
