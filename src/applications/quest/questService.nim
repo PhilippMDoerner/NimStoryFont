8f87ed28cf0c8d0f2ae25e2b68baafce38de8137
Philipp Doerner <show>
2022-03-26 18:13:23 +0100

    Added Quest API Endpoints
    closes #27

--- src/applications/quest/questService.nim
+++ src/applications/quest/questService.nim
@@ -6,3 +6,11 @@ export questModel
 proc getQuestById*(questId: int64): Quest =
     result = getEntryById(questId, Quest)
 
+proc getQuestSerialization*(connection: DbConn, entry: Quest): Quest =
+    result = entry
+
+proc getCampaignQuestList*(campaignName: string): seq[Quest] =
+    result = getCampaignList(campaignName, Quest)
+
+proc getQuestByName*(campaignName: string, entryName: string): Quest =
+    result = getEntryByName(campaignName, entryName, Quest)
