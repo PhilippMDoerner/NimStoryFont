5bbd3e0ca9f446afa1ada3fed72fe98983a7aebc
Philipp Doerner <show>
2022-03-26 09:48:42 +0100

    Adjusted templates

--- src/applications/controllerTemplates.nim
+++ src/applications/controllerTemplates.nim
@@ -9,15 +9,12 @@ template respondBadRequestOnDbError*(body: untyped) =
       body
 
   except DbError:
-    debug("Error during db request", getCurrentException().name, getCurrentExceptionMsg(), getStackTraceEntries()) 
+    debug("Error during db request ", getCurrentException().name, getCurrentExceptionMsg(), getStackTraceEntries()) 
     resp get400BadRequestResponse(getCurrentExceptionMsg())
-    raise
 
   except NotFoundError: #grep -Hnr "NotFoundError" ~/.nimble/pkgs
-    debug("Error during db request", getCurrentException().name, getCurrentExceptionMsg(), getStackTraceEntries()) 
+    debug("Error during db request ", getCurrentException().name, getCurrentExceptionMsg(), getStackTraceEntries()) 
     resp get404NotFoundResponse() 
-    raise
 
   except Exception:
-    debug("Error during db request", getCurrentException().name, getCurrentExceptionMsg(), getStackTraceEntries()) 
-    raise
+    debug("Error during db request ", getCurrentException().name, getCurrentExceptionMsg(), getStackTraceEntries()) 
