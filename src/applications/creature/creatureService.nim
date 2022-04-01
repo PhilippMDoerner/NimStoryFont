c72cceaecd34d344ac4bad9ef281a17043cff90d
Philipp Doerner <show>
2022-04-01 22:32:14 +0200

    Massive creature refactor

--- src/applications/creature/creatureService.nim
+++ src/applications/creature/creatureService.nim
@@ -1,21 +1,45 @@
 import ../genericArticleRepository
+import ../genericArticleService
 import creatureModel
+import creatureSerialization
 import norm/sqlite
+import ../allUrlParams
+import ../../utils/databaseUtils
+import std/[sugar, sequtils]
+
 export creatureModel
+export creatureSerialization
 
-proc getCampaignCreatureListOverview*(campaignName: string): seq[CreatureOverview] =
-    ## lists all campaign entries using a limited but performant representation of a Creature
-    result = getCampaignList(campaignName, CreatureOverview)
+proc getCampaignCreatureList*(requestParams: ReadListParams): seq[CreatureOverviewSerializable] =
+    ## lists all campaign entries using a limited but performant representation of a character
+    withDbConn(connection):
+        let entries = connection.getCampaignList(requestParams.campaignName, CreatureOverview)
+        result = entries.map(entry => connection.overviewSerialize(entry))
 
+proc getCreatureById*(requestParams: ReadByIdParams): CreatureSerializable =
+    withDbConn(connection):
+        result = connection.serialize(requestParams.id)
 
-proc getCreatureByName*(campaignName: string, entryName: string): CreatureRead = 
-    result = getEntryByName(campaignName, entryName, CreatureRead)
+proc getCreatureByName*(requestParams: ReadByNameParams): CreatureSerializable =
+    withDbConn(connection):
+        result = connection.serialize(requestParams.campaignName, requestParams.articleName)
 
+proc updateCreature*(requestParams: UpdateParams): CreatureSerializable =
+    withDbTransaction(connection):
+        let updatedEntry = connection.updateArticle(requestParams.id, requestParams.body, Creature)
+        result = connection.serialize(updatedEntry.id)
+        
+proc deleteCreature*(requestParams: DeleteParams) =
+    deleteEntry(requestParams.id, Creature)
 
-proc getCreatureById*(entryId: int64): CreatureRead {.gcsafe.}=
-    result = getEntryById(entryId, CreatureRead)
+proc getCreatureList*(requestParams: ReadListParams): seq[CreatureOverviewSerializable] =
+    withDbConn(connection):
+        let entries = connection.getCampaignList(requestParams.campaignName, CreatureOverview)
+        result = entries.map(entry => connection.overviewSerialize(entry))
 
+proc createCreature*(requestParams: CreateParams): CreatureSerializable =
+    withDbTransaction(connection):
+        let createdEntry = connection.createArticle(requestParams.body, Creature)
+        result = connection.serialize(createdEntry.id)
 
-proc getCreatureSerialization*(connection: sqlite.DbConn, entry: Creature): CreatureRead {.gcsafe.}=
-    result = connection.getEntryById(entry.id, CreatureRead)
 
