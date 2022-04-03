ecc4a8b08c0a3d872f649df2420efa630397595c
Philipp Doerner <show>
2022-04-03 15:12:34 +0200

    Removed unused procs

--- src/applications/diaryentry/diaryEntryService.nim
+++ src/applications/diaryentry/diaryEntryService.nim
@@ -12,31 +12,8 @@ import ../allUrlParams
 export diaryEntryModel
 export diaryEntrySerialization
 
-proc getDiaryEntryById*(requestParams: ReadByIdParams): DiaryEntrySerializable =
-  withDbConn(connection):
-    let entry = connection.getEntryById(requestParams.id, DiaryEntry)
-    result = connection.serialize(entry)
-
-proc getDiaryEntryList*(requestParams: ReadListParams): seq[DiaryEntryOverviewSerializable] =
-  let entries: seq[DiaryEntryRead] = getDiaryEntriesForCampaign(requestParams.campaignName)
-  withDbConn(connection):
-    result = entries.map(entry => connection.overviewSerialize(entry))
-
-proc createDiaryEntry*(requestParams: CreateParams): DiaryEntrySerializable =
-  withDbTransaction(connection):
-    let newEntry = connection.createArticle(requestParams.body, DiaryEntry)
-    result = connection.serialize(newEntry.id)
-  
-proc updateDiaryEntry*(requestParams: UpdateParams): DiaryEntrySerializable =
-  withDbTransaction(connection):
-    let updatedEntry = connection.updateArticle(requestParams.id, requestParams.body, DiaryEntry)
-    result = connection.serialize(updatedEntry.id)
-
-proc deleteDiaryEntry*(requestParams: DeleteParams) =
-  deleteEntry(requestParams.id, DiaryEntry)
-
 proc getDiaryEntry*(connection: DbConn, requestParams: ReadDiaryEntryParams): DiaryEntrySerializable =
-  let entry = diaryEntryRepository.getDairyEntry(
+  result = diaryEntryRepository.getDairyEntry(
     connection,
     requestParams.campaignName, 
     requestParams.sessionNumber, 
