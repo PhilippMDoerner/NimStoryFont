531ef125239e7b98e6be9875e4c52e15c15e2e59
PhilippMDoerner <philippmdoerner@web.de>
2022-04-03 12:43:44 +0200

    Removed unnecessary serialization procs

--- src/applications/creature/creatureSerialization.nim
+++ src/applications/creature/creatureSerialization.nim
@@ -10,20 +10,15 @@ type CreatureOverviewSerializable* = CreatureOverview
 proc serialize*(connection: DbConn, entry: CreatureRead): CreatureSerializable =
     result = entry
 
-proc serialize*(connection: DbConn, entryId: int64): CreatureSerializable =
-    let entry = connection.getEntryById(entryId, CreatureRead)
-    result = connection.serialize(entry)
-
-proc serialize*(connection: DbConn, campaignName: string, entryName: string): CreatureSerializable =
-    let entry = connection.getEntryByName(campaignName, entryName, CreatureRead)
-    result = connection.serialize(entry)
+proc serialize*(connection: DbConn, entry: Creature): CreatureSerializable =
+    let fullEntry = connection.getEntryById(entry.id, CreatureRead)
+    result = connection.serialize(fullEntry)
 
 proc overviewSerialize*(connection: DbConn, entry: CreatureOverview): CreatureOverviewSerializable =
     result = entry
 
 
-proc serialize*(connection: DbConn, entry: Creature): CreatureSerializable =
-    let fullEntry = connection.getEntryById(entry.id, CreatureRead)
-    result = connection.serialize(fullEntry)
-
-
+#TODO: Figure out how to delete this proc, it currently only exists because otherwise shit doesn't compile
+proc serialize(connection: DbConn, campaignName: string, entryName: string): CreatureSerializable =
+    let entry = connection.getEntryByName(campaignName, entryName, CreatureRead)
+    result = connection.serialize(entry)
