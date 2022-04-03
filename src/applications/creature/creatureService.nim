6550b788460d9095d59f045bf8b3df34ce0cfdd2
PhilippMDoerner <philippmdoerner@web.de>
2022-04-03 12:33:06 +0200

    made unused proc private

--- src/applications/creature/creatureService.nim
+++ src/applications/creature/creatureService.nim
@@ -11,5 +11,5 @@ export creatureSerialization
 #TODO: For some unknown reason this is required or nothing compiles ?! 
 #Current hypothesis: The fact that a generic handler creator creates a generic handler calling a generic service calling a generic repository
 #May lead to issues regarding instantiation and somehow that may lead to things not compiling if not explicitly called at least once (?)
-proc getCreatureList*(connection: DbConn, requestParams: ReadListParams): seq[CreatureOverviewSerializable] =
+proc getCreatureList(connection: DbConn, requestParams: ReadListParams): seq[CreatureOverviewSerializable] =
     result = readCampaignArticleList[ReadListParams, CreatureOverview](connection, requestParams)
