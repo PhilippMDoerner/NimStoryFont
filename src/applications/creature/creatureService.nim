2e22de0b0e0acb4a5d079c4b601789fd9104aa93
Philipp Doerner <show>
2022-04-03 12:32:29 +0200

    Removed most unnecessary procs

--- src/applications/creature/creatureService.nim
+++ src/applications/creature/creatureService.nim
@@ -4,31 +4,12 @@ import creatureModel
 import creatureSerialization
 import norm/sqlite
 import ../allUrlParams
-import ../../utils/databaseUtils
-import std/[sugar, sequtils]
 
 export creatureModel
 export creatureSerialization
 
-proc getCampaignCreatureList*(requestParams: ReadListParams): seq[CreatureOverviewSerializable] =
-    ## lists all campaign entries using a limited but performant representation of a character
-    withDbConn(connection):
-        let entries = connection.getCampaignList(requestParams.campaignName, CreatureOverview)
-        result = entries.map(entry => connection.overviewSerialize(entry))
-
-proc getCreatureById*(connection: DbConn, requestParams: ReadByIdParams): Creature =
-    result = connection.getEntryById(requestParams.id, Creature)
-
-proc getCreatureById*(connection: DbConn, requestParams: UpdateParams): Creature =
-    result = connection.getEntryById(requestParams.id, Creature)
-
-proc getCreatureByName*(connection: DbConn, requestParams: ReadByNameParams): CreatureRead =
-    result = connection.getEntryByName(requestParams.campaignName, requestParams.articleName, CreatureRead)
-
-proc deleteCreature*(connection: DbConn, entry: var Creature)=
-    connection.deleteEntryInTransaction(entry)
-
+#TODO: For some unknown reason this is required or nothing compiles ?! 
+#Current hypothesis: The fact that a generic handler creator creates a generic handler calling a generic service calling a generic repository
+#May lead to issues regarding instantiation and somehow that may lead to things not compiling if not explicitly called at least once (?)
 proc getCreatureList*(connection: DbConn, requestParams: ReadListParams): seq[CreatureOverviewSerializable] =
-    result = connection.getCampaignList(requestParams.campaignName, CreatureOverview)
-
-
+    result = readCampaignArticleList[ReadListParams, CreatureOverview](connection, requestParams)
