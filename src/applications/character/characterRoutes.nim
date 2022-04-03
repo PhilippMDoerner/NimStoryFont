35b08baff40757b256712bfda3603d82bea0dd2e
PhilippMDoerner <philippmdoerner@web.de>
2022-04-03 13:33:29 +0200

    Refactor of character

--- src/applications/character/characterRoutes.nim
+++ src/applications/character/characterRoutes.nim
@@ -3,48 +3,49 @@ import ../../middleware/loginMiddleware
 import ../allUrlParams
 import characterModel
 import characterService
+import characterSerialization
 import std/strformat
 import ../genericArticleControllers
 
 proc addCharacterRoutes*(app: Prologue) =
     app.addRoute(
         re"/character/",
-        handler = createSimpleHandler(CreateParams, createCharacter),
+        handler = createCreateArticleHandler[CreateParams, Character, CharacterSerializable](serializeCharacter),
         httpMethod = HttpPost,
         middlewares = @[loginMiddleware()]
     )
 
     app.addRoute(
         re fmt"/character/{ID_PATTERN}/", 
-        handler = createSimpleDeletionHandler(DeleteParams, deleteCharacter),
+        handler = createDeleteByIdHandler[DeleteParams, Character](),
         httpMethod = HttpDelete,
         middlewares = @[loginMiddleware()]
     )
 
     app.addRoute(
         re fmt"/character/{ID_PATTERN}/", 
-        handler = createSimpleHandler(UpdateParams, updateCharacter),
+        handler = createUpdateByIdHandler[UpdateParams, Character, CharacterSerializable](serializeCharacter),
         httpMethod = HttpPut,
         middlewares = @[loginMiddleware()]
     )
 
     app.addRoute(
         re fmt"/character/{ID_PATTERN}/", 
-        handler = createSimpleHandler(ReadByIdParams, getCharacterById), 
+        handler = createReadByIdHandler[ReadByIdParams, CharacterRead, CharacterSerializable](serializeCharacterRead), 
         httpMethod = HttpGet,
         middlewares = @[loginMiddleware()]
     )
 
     app.addRoute(
         re fmt"/character/{CAMPAIGN_NAME_PATTERN}/overview/", 
-        handler = createSimpleHandler(ReadListParams, getCharacterList),
+        handler = createReadCampaignListHandler[ReadListParams, CharacterOverview, CharacterOverviewSerializable](serializeCharacterOverview),
         httpMethod = HttpGet,
         middlewares = @[loginMiddleware()]
     )
     
     app.addRoute(
         re fmt"/character/{CAMPAIGN_NAME_PATTERN}/{ARTICLE_NAME_PATTERN}/", 
-        handler = createSimpleHandler(ReadByNameParams, getCharacterByName),  
+        handler = createReadByNameHandler[ReadByNameParams, CharacterRead, CharacterSerializable](serializeCharacterRead),  
         httpMethod = HttpGet,
         middlewares = @[loginMiddleware()]
     )
