46ce0d22f0d794a069bb0f3d3d679807e2c4ee45
PhilippMDoerner <philippmdoerner@web.de>
2022-04-07 13:01:17 +0200

    #36 Refactored spell controllers and removed unnecessary service procs

--- src/applications/session/sessionService.nim
+++ src/applications/session/sessionService.nim
@@ -7,14 +7,5 @@ import ../allUrlParams
 export genericArticleRepository
 export sessionModel
 
-proc getSessionById*(sessionId: int64): SessionRead =
-    result = getEntryById(sessionId, SessionRead)
-
-proc getCampaignSessionListOverview*(campaignName: string): seq[Session] =
-    result = getCampaignList(campaignName, Session)
-
-proc getSerializedSession*(connection: DbConn, entry: Session): SessionRead =
-    result = connection.getEntryById(entry.id, SessionRead)
-
 proc getSessionByParams*(connection: DbConn, requestParams: ReadSessionByParams): SessionRead =
     result = connection.getSession(requestParams.campaignName, requestParams.sessionNumber, requestParams.isMainSession.bool)
--- src/applications/spell/spellService.nim
+++ src/applications/spell/spellService.nim
@@ -2,15 +2,3 @@ import spellModel
 import ../genericArticleRepository
 
 export spellModel
-
-proc getSpellById*(spellId: int64): Spell =
-    result = getEntryById(spellId, Spell)
-
-proc getSpellSerialization*(connection: DbConn, entry: Spell): Spell =
-    result = entry
-
-proc getCampaignSpellList*(campaignName: string): seq[Spell] =
-    result = getCampaignList(campaignName, Spell)
-
-proc getSpellByName*(campaignName: string, entryName: string): Spell =
-    result = getEntryByName(campaignName, entryName, Spell)
