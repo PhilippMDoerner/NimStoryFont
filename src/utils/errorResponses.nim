7b7eef0d7d307027ce6acdab3f124c17d966c9af
Philipp Doerner <show>
2022-04-07 19:45:01 +0200

    Added handling 403 responses

--- src/utils/errorResponses.nim
+++ src/utils/errorResponses.nim
@@ -7,7 +7,7 @@ proc get400BadRequestResponse*(errorMsg: string): Response {.inline.}  =
     let responseText = "Something around the given data was invalid: " & errorMsg
     result = jsonResponse(%*responseText, Http400)
 
-proc get403ForbiddenResponse*(ctx: Context): Response {.inline.} =
+proc get403ForbiddenResponse*(): Response {.inline.} =
     result = jsonResponse(%*"You lack access rights for the requested action", Http403)
 
 proc get404NotFoundResponse*(): Response {.inline.} =
