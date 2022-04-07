5ff5a0d8f8b17eb36764baf3adf810bd072dc17b
Philipp Doerner <show>
2022-04-07 10:19:40 +0200

    #36 refactored session controllers

--- src/applications/session/sessionRoutes.nim
+++ src/applications/session/sessionRoutes.nim
@@ -1,7 +1,7 @@
 import prologue except Session
 import ../../middleware/[loginMiddleware]
 import ../allUrlParams
-import sessionControllers
+import sessionSerialization
 import sessionModel
 import sessionService
 import ../genericArticleControllers
@@ -10,35 +10,35 @@ import std/strformat
 proc addSessionRoutes*(app: Prologue) =
     app.addRoute(
         re"/session/",
-        handler = createEntryCreationHandler(Session, getSerializedSession),
+        handler = createCreateArticleHandler[CreateParams, Session, SessionSerializable](serializeSession),
         httpMethod = HttpPost,
         middlewares = @[loginMiddleware()]
     )
 
     app.addRoute(
         re fmt"/session/{ID_PATTERN}/", 
-        handler = createEntryDeletionHandler(Session, ID_PARAM),
+        handler = createDeleteByIdHandler[DeleteParams, Session](),
         httpMethod = HttpDelete,
         middlewares = @[loginMiddleware()]
     )
 
     app.addRoute(
         re fmt"/session/{ID_PATTERN}/",
-        handler = createEntryUpdateHandler(Session, ID_PARAM, getSerializedSession),
+        handler = createUpdateByIdHandler[UpdateParams, Session, SessionSerializable](serializeSession),
         httpMethod = HttpPut,
         middlewares = @[loginMiddleware()]
     )
 
     app.addRoute(
         re fmt"/session/{CAMPAIGN_NAME_PATTERN}/overview/", 
-        handler = createCampaignOverviewHandler(CAMPAIGN_NAME_PARAM, getCampaignSessionListOverview),  
+        handler = createReadCampaignListHandler[ReadListParams, SessionRead, SessionSerializable](serializeSessionRead),  
         httpMethod = HttpGet,
         middlewares = @[loginMiddleware()]
     )
 
     app.addRoute(
         re fmt"/session/{CAMPAIGN_NAME_PATTERN}/{SESSION_NUMBER_PATTERN}/{SESSION_IS_MAIN_SESSION_PATTERN}/", 
-        handler = getSessionByParamsController,  
+        handler = createReadHandler[ReadSessionByParams, SessionRead, SessionSerializable](getSessionByParams, checkReadPermission, serializeSessionRead),  
         httpMethod = HttpGet,
         middlewares = @[loginMiddleware()]
     )
