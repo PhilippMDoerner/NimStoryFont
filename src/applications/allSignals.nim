edb1a5276d915dbd8963e514e9d56e257b74736c
PhilippMDoerner <philippmdoerner@web.de>
2022-04-10 17:17:30 +0200

    Added ability to delete an audio file if the entry gets deleted

--- src/applications/allSignals.nim
+++ src/applications/allSignals.nim
@@ -1,4 +1,5 @@
 import search/searchSignals
 import campaign/campaignSignals
 import image/imageSignals
+import sessionaudio/sessionaudioSignals
 ## A hub for all signals. Just import them here to ensure their global code is executed
---
+++ src/applications/sessionaudio/sessionaudioSignals.nim
@@ -0,0 +1,17 @@
+import ../core/signalSystem
+import ../../applicationSettings
+import ../../utils/fileUpload
+import std/[os, json, strformat]
+import norm/sqlite
+import sessionaudioModel
+import prologue
+
+proc deleteSessionAudioFile(connection: DbConn, modelInstance: SessionAudio) =
+  ## Deletes an sessionAudio file off the harddrive if the corresponding sessionAudio entry
+  ## in the database is deleted
+  let sessionAudioFilepath: string = modelInstance.audio_file
+  let mediaDirectory: string = settings["audioDir"].getStr()
+  echo fmt"Audio File Deleting {sessionAudioFilepath} from {mediaDirectory}"
+  deleteArticleImage(sessionAudioFilepath, mediaDirectory)
+
+connect(SignalType.stPreDelete, SessionAudio, deleteSessionAudioFile)
