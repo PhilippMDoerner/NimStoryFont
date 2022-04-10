9619d0b89904136222b69f99d0dde832c88e972a
PhilippMDoerner <philippmdoerner@web.de>
2022-04-10 23:15:43 +0200

    Added stuff for session audio

--- src/applications/sessionaudio/sessionaudioControllers.nim
+++ src/applications/sessionaudio/sessionaudioControllers.nim
@@ -7,8 +7,7 @@ import ../../utils/[jwtContext, customResponses, errorResponses, databaseUtils]
 import jsony
 import ../authentication/authenticationUtils
 import ../controllerTemplates
-import ../allUrlParams
-import ../../utils/djangoDateTime/[serialization]
+import ../../utils/djangoDateTime/[djangoDateTimeType, serialization]
 import ../genericArticleRepository
 
 
@@ -31,7 +30,8 @@ proc createSessionAudioControlloer*(ctx: Context) {.async, gcsafe.}=
     var sessionaudioFormData = SessionAudioDTO(
         sessionaudioFile: ctx.extractFileFromContext("audio_file"),
         audioDirectory: audioDirectory,
-        sessionId: sessionId,
+        sessionId: some(sessionId),
+        entryId: none(int64)
     )
 
     respondBadRequestOnDbError():
@@ -43,6 +43,76 @@ proc createSessionAudioControlloer*(ctx: Context) {.async, gcsafe.}=
 
         else:
           resp get400BadRequestResponse("The sent sessionaudio could not be saved, because there was no sessionaudio file in the sent form under the 'sessionaudio' key.")
+
+type PatchSessionOfAudio* = object
+  session: int64
+  pk: int64
+  update_datetime: DjangoDateTime
+
+proc parseJSONPatchBody(ctx: JWTContext): SessionAudioDTO =
+  let parsedBody = ctx.request.body().fromJson(PatchSessionOfAudio)
+  let newSessionId: int64 = parsedBody.session
+
+  result = SessionAudioDTO(
+      sessionaudioFile: none(UpLoadFile),
+      audioDirectory: ctx.getSettings("audioDir").getStr(),
+      sessionId: some(newSessionId),
+      entryId: some(parsedBody.pk)
+  )
+
+proc parseFormPatchBody(ctx: JWTContext): SessionAudioDTO =
+  let sessionId: int64 = ctx.getFormParamsOption("session").get().parseInt().int64
+  let entryId: int64 = ctx.getFormParamsOption("sessionaudio").get().parseInt().int64
+  
+  result = SessionAudioDTO(
+      sessionaudioFile: ctx.extractFileFromContext("audio_file"),
+      audioDirectory: ctx.getSettings("audioDir").getStr(),
+      sessionId: some(sessionId),
+      entryId: some(entryId)
+  )
+
+proc patchSessionAudioController*(ctx: Context) {.async, gcsafe.}=
+    let ctx = JWTContext(ctx)
+
+    let isFormRequest = ctx.extractFileFromContext("audio_file").isSome()
+    var sessionaudioUpdateParams = if isFormRequest: ctx.parseFormPatchBody() else: ctx.parseJSONPatchBody()
+
+    respondBadRequestOnDbError():
+      withDbTransaction(connection):
+        var oldEntry: SessionAudio = connection.getEntryById(sessionaudioUpdateParams.entryId.get(), SessionAudio)
+        checkUpdatePermission(ctx, oldEntry)
+
+        let updatedEntry = connection.patchSessionAudio(sessionaudioUpdateParams, oldEntry)
+        let data = connection.serializeSessionAudio(updatedEntry)
+        resp jsonyResponse(ctx, data)
+
+
+
+
+# proc createSessionAudioControlloer*(ctx: Context) {.async, gcsafe.}=
+#     let ctx = JWTContext(ctx)
+
+#     let campaignId: int64 = ctx.getFormParamsOption("campaign").get().parseInt().int64
+#     checkCreatePermission(ctx, campaignId)
+
+#     let sessionId: int64 = ctx.getFormParamsOption("session").get().parseInt().int64
+#     let audioDirectory: string = ctx.getSettings("audioDir").getStr()
+
+#     var sessionaudioFormData = SessionAudioDTO(
+#         sessionaudioFile: ctx.extractFileFromContext("audio_file"),
+#         audioDirectory: audioDirectory,
+#         sessionId: sessionId,
+#     )
+
+#     respondBadRequestOnDbError():
+#       withDbTransaction(connection):
+#         let newEntry: Option[SessionAudioRead] = connection.createSessionAudio(sessionaudioFormData)
+#         if newEntry.isSome():
+#           let data = connection.serializeSessionAudioRead(newEntry.get())
+#           resp jsonyResponse(ctx, data)
+
+#         else:
+#           resp get400BadRequestResponse("The sent sessionaudio could not be saved, because there was no sessionaudio file in the sent form under the 'sessionaudio' key.")
 
 
 # proc updateImageView*(ctx: Context) {.async, gcsafe.} =
