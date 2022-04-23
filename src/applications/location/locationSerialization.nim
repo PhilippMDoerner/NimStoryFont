35c6dd0f8f5d4d27c8c9a413d4a9d6023e8f2f72
Philipp Doerner <show>
2022-04-23 17:27:53 +0200

    #32 Implemented location serialization

--- src/applications/location/locationSerialization.nim
+++ src/applications/location/locationSerialization.nim
@@ -1,15 +1,150 @@
 import norm/model
 import locationModel
+import locationUtils
+import locationRepository
+import ../image/imageModel
+import ../mapMarker/[markerModel, markerSerialization]
+import ../character/[characterModel, characterSerialization, characterUtils]
+import ../campaign/campaignModel
 import ../genericArticleRepository
+import ../../utils/djangoDateTime/djangoDateTimeType
+import std/[options, sugar, sequtils]
 
-type LocationSerializable* = LocationRead
-type LocationOverviewSerializable* = LocationRead
+type LocationCharacter* = object
+    pk*: int64
+    name*: string
+    name_full*: string
 
-proc serializeLocation*(connection: DbConn, entry: Location): LocationSerializable =
-    result = connection.getEntryById(entry.id, LocationRead)
+proc serializeLocationCharacter(entry: Character): LocationCharacter =
+    result = LocationCharacter(
+        pk: entry.id,
+        name: entry.name,
+        name_full: $entry
+    )
+
+type LocationMarker* = object
+    map: string
+    map_icon: Option[string]
+
+proc serializeLocationMarker(entry: MarkerRead): LocationMarker =
+    result = LocationMarker(map: entry.map_id.name, map_icon: entry.icon)
+
+type ParentLocationSerializable* = object
+    pk*: int64
+    name*: string
+    name_full*: string
+    partent_location*: Option[string]
+
+proc serializeParentLocation(entry: ParentLocation | Location): ParentLocationSerializable =
+    result = ParentLocationSerializable(
+        pk: entry.id,
+        name: entry.name,
+        name_full: entry.name,
+        partent_location: some("")
+    )
+
+
+type SubLocationSerializable* = object
+    creation_datetime: DjangoDateTime
+    update_datetime: DjangoDateTime
+    pk: int64
+    name: string
+    description: Option[string]
+    parent_location: Option[int64]
+    parent_location_details: Option[ParentLocationSerializable]
+    parent_location_list: seq[string]
+    name_full: string
+    characters: seq[LocationCharacter]
+    marker_details: seq[LocationMarker]
+    campaign: int64
+    campaign_details: MinimumCampaignOverview
+
+proc serializeSubLocation(connection: DbConn, entry: LocationRead): SubLocationSerializable =
+    let parentLocationNames: seq[string] = connection.getParentLocations(entry.id).map(loc => loc.name)
+    let characters: seq[LocationCharacter] = connection.getManyFromOne(entry, Character).map(serializeLocationCharacter)
+    let markers: seq[LocationMarker] = connection.getManyFromOne(entry, MarkerRead)
+        .map(serializeLocationMarker)
+
+    result = SubLocationSerializable(
+        creation_datetime: entry.creation_datetime,
+        update_datetime: entry.update_datetime,
+        pk: entry.id,
+        name: entry.name,
+        name_full: $entry,
+        description: entry.description,
+        parent_location: entry.parent_location_id.map(ploc => ploc.id),
+        parent_location_details: entry.parent_location_id.map(serializeParentLocation),
+        parent_location_list: parentLocationNames,
+        characters: characters,
+        marker_details: markers,
+        campaign: entry.campaign_id.id,
+        campaign_details: entry.campaign_id
+    )
+
+type LocationSerializable* = object
+    creation_datetime*: DjangoDateTime
+    update_datetime*: DjangoDateTime
+    pk*: int64
+    name*: string
+    name_full*: string
+    description*: Option[string]
+    parent_location*: Option[int64]
+    parent_location_details*: Option[ParentLocationSerializable]
+    parent_location_list*: seq[string]
+    images*: seq[Image]
+    sublocations*: seq[SubLocationSerializable]
+    characters*: seq[LocationCharacter]
+    marker_details*: seq[LocationMarker]
+    campaign*: int64
+    campaign_details*: MinimumCampaignOverview
 
 proc serializeLocationRead*(connection: DbConn, entry: LocationRead): LocationSerializable =
-    result = entry
+    let images: seq[Image] = connection.getManyFromOne(entry, Image)
+    let parentLocations: seq[ParentLocationSerializable] = connection.getParentLocations(entry.id)
+        .map(serializeParentLocation)
+    let sublocations: seq[SubLocationSerializable] = connection.getManyFromOne(entry, LocationRead)
+        .map(sloc => connection.serializeSubLocation(sloc))
+    let characters: seq[LocationCharacter] = connection.getManyFromOne(entry, Character).map(serializeLocationCharacter)
+    let markers: seq[LocationMarker] = connection.getManyFromOne(entry, MarkerRead)
+        .map(serializeLocationMarker)
+
+    result = LocationSerializable(
+        creation_datetime: entry.creation_datetime,
+        update_datetime: entry.update_datetime,
+        pk: entry.id,
+        name: entry.name,
+        name_full: $entry,
+        description: entry.description,
+        parent_location: entry.parent_location_id.map(ploc => ploc.id),
+        parent_location_details: entry.parent_location_id.map(serializeParentLocation),
+        parent_location_list: parentLocations.map(ploc => ploc.name),
+        images: images,
+        sublocations: sublocations,
+        characters: characters,
+        marker_details: markers,
+        campaign: entry.campaign_id.id,
+        campaign_details: entry.campaign_id
+    )
+
+proc serializeLocation*(connection: DbConn, entry: Location): LocationSerializable =
+    let locationRead = connection.getEntryById(entry.id, LocationRead)
+    result = connection.serializeLocationRead(locationRead)
+
+
+type LocationOverviewSerializable* = object
+    pk: int64
+    name_full: string
+    name: string
+    campaign_details: MinimumCampaignOverview
+    update_datetime: DjangoDateTime
+    parent_location_details: Option[ParentLocationSerializable]
 
 proc overviewSerialize*(connection: DbConn, entry: LocationRead): LocationOverviewSerializable =
-    result = entry
+    result = LocationOverviewSerializable(
+        pk: entry.id,
+        name_full: entry.name,
+        name: entry.name,
+        campaign_details: entry.campaign_id,
+        update_datetime: entry.update_datetime,
+        parent_location_details: entry.parent_location_id.map(serializeParentLocation)
+    )
