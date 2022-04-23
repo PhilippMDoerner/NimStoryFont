ee161fc4a4aaf117de4efc2a21bd39b6fd504dd9
PhilippMDoerner <philippmdoerner@web.de>
2022-04-23 21:45:21 +0200

    Removed useless proc

--- src/applications/organization/organizationService.nim
+++ src/applications/organization/organizationService.nim
@@ -5,15 +5,3 @@ import ../image/[imageModel, imageService]
 import tinypool
 
 export organizationModel
-
-
-proc getOrganizationById*(organizationId: int64): OrganizationSerializable =
-    let organization = getEntryById(organizationId, OrganizationRead)
-    let images = getArticleImage(ImageType.ORGANIZATIONTYPE, organizationId)
-    let members = getManyFromOne(organization, OrganizationCharacter)
-
-    result = OrganizationSerializable(
-        organization: organization,
-        images: images,
-        members: members
-    )
