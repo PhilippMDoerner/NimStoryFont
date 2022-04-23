9fc42ac349163331d35c3ff191b8a8a4ccc89016
PhilippMDoerner <philippmdoerner@web.de>
2022-04-23 20:27:10 +0200

    Cut down unnecessary serialization code

--- src/applications/mapMarkerType/markerTypeSerialization.nim
+++ src/applications/mapMarkerType/markerTypeSerialization.nim
@@ -2,14 +2,8 @@ import markerTypeModel
 import norm/sqlite
 import ../genericArticleRepository
 
-type MarkerTypeRead* = MarkerType
-type MarkerTypeSerializable* = MarkerTypeRead
+type MarkerTypeSerializable* = MarkerType
 
-proc serializeMarkerTypeRead*(connection: DbConn, entry: MarkerTypeRead): MarkerTypeSerializable =
-    result = entry
-
-proc serializeMarkerType*(connection: DbConn, entry: MarkerType): MarkerTypeSerializable =
-    let fullEntry = connection.getEntryById(entry.id, MarkerTypeRead)
-    result = connection.serializeMarkerTypeRead(fullEntry)
+proc serializeMarkerType*(connection: DbConn, entry: MarkerType): MarkerTypeSerializable = entry
 
 
