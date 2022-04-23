7a281119a6c75ca05df39d9057c875d6711f9dc2
Philipp Doerner <show>
2022-04-23 17:16:55 +0200

    Added util to stringify locationread and made it more efficient

--- src/applications/location/locationUtils.nim
+++ src/applications/location/locationUtils.nim
@@ -2,8 +2,8 @@ import locationService
 import std/[strformat]
 
 
-proc `$`*(model: Location): string = 
-    var parentLocations: seq[Location] = getParentLocations(model)
+proc `$`*(model: Location | LocationRead): string = 
+    var parentLocations: seq[Location] = readParentLocations(model)
 
     for parentLocation in parentLocations:
         result.add(fmt "{parentLocation.name} - ")
