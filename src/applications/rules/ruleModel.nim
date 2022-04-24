fce5bfe3a7b627290cca1d74c21f04b6cce40f30
PhilippMDoerner <philippmdoerner@web.de>
2022-04-24 12:31:09 +0200

    #32 added Rule serialization

--- src/applications/rules/ruleModel.nim
+++ src/applications/rules/ruleModel.nim
@@ -7,13 +7,23 @@ import constructor/defaults
 import norm/[model, pragmas]
 
 type Rule* {.defaults, tableName: RULES_TABLE.} = ref object of Model
-    ##[TableModel of the table of story characters. ]##
     name*: string = ""
     description*: Option[string] = none(string) # A description of the character
     creation_datetime*: DjangoDateTime = djangoDateTimeType.now()
     update_datetime*: DjangoDateTime = djangoDateTimeType.now()
-    campaign_id* {.fk: Campaign.}: int64 = MODEL_INIT_ID # The id of the campaign that this character occurred in
+    campaign_id* {.fk: Campaign.}: int64 = MODEL_INIT_ID 
 
 implDefaults(Rule)
 
 proc newModel*(T: typedesc[Rule]): Rule = newRule()
+
+type RuleRead* {.defaults, tableName: RULES_TABLE.} = ref object of Model
+    name*: string = ""
+    description*: Option[string] = none(string) # A description of the character
+    creation_datetime*: DjangoDateTime = djangoDateTimeType.now()
+    update_datetime*: DjangoDateTime = djangoDateTimeType.now()
+    campaign_id*: MinimumCampaignOverview = newModel(MinimumCampaignOverview)
+
+implDefaults(RuleRead)
+
+proc newModel*(T: typedesc[RuleRead]): RuleRead = newRuleRead()
--- src/applications/rules/ruleSerialization.nim
+++ src/applications/rules/ruleSerialization.nim
@@ -1,13 +1,30 @@
 import ruleModel
 import norm/sqlite
 import ../genericArticleRepository
+import ../campaign/campaignModel
+import ../../utils/djangoDateTime/[djangoDateTimeType]
+import std/[options, sugar]
 
-type RuleRead* = Rule
-type RuleSerializable* = RuleRead
+type RuleSerializable* = object
+    pk: int64
+    creation_datetime: DjangoDateTime
+    update_datetime: DjangoDateTime
+    description: Option[string]
+    name: string
+    campaign: int64
+    campaign_details: MinimumCampaignOverview
 
 
 proc serializeRuleRead*(connection: DbConn, entry: RuleRead): RuleSerializable =
-    result = entry
+    result = RuleSerializable(
+        pk: entry.id,
+        creation_datetime: entry.creation_datetime,
+        update_datetime: entry.update_datetime,
+        description: entry.description,
+        name: entry.name,
+        campaign: entry.campaign_id.id,
+        campaign_details: entry.campaign_id
+    )
 
 proc serializeRule*(connection: DbConn, entry: Rule): RuleSerializable =
     let fullEntry = connection.getEntryById(entry.id, RuleRead)
