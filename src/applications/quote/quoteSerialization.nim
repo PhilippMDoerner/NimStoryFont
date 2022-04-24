f0c0b4baacdf7e51f0da7a3b22e3588e5aed5b15
Philipp Doerner <show>
2022-04-24 11:44:01 +0200

    #32 implemented Quote Serialization

--- src/applications/quote/quoteSerialization.nim
+++ src/applications/quote/quoteSerialization.nim
@@ -1,18 +1,67 @@
 import quoteModels
 import norm/sqlite
+import ../session/[sessionSerialization, sessionModel]
 import ../genericArticleRepository
+import ../character/characterUtils
+import ../../utils/djangoDateTime/djangoDateTimeType
+import std/[options, sequtils, sugar]
 
-type QuoteSerializable* = QuoteRead
-type QuoteConnectionSerializable* = QuoteConnectionRead
+type QuoteConnectionCharacter* = object
+    name: string
+    name_full: string
+    pk: int64
+
+type QuoteConnectionSerializable* = object
+    pk: int64
+    character: int64
+    character_details: QuoteConnectionCharacter
+    quote: int64
+
+proc serializeQuoteConnectionRead(entry: QuoteConnectionRead): QuoteConnectionSerializable =
+    result = QuoteConnectionSerializable(
+        pk: entry.id,#
+        character: entry.character_id.id,
+        quote: entry.quote_id.id,
+        character_details: QuoteConnectionCharacter(
+            pk: entry.character_id.id,
+            name: entry.character_id.name,
+            name_full: $entry.character_id
+        )
+    )
+
+proc serializeQuoteConnectionRead*(connection: DbConn, entry: QuoteConnectionRead): QuoteConnectionSerializable =
+    result = serializeQuoteConnectionRead(entry)
+
+type QuoteSerializable* = object
+    pk: int64
+    quote: string
+    description: Option[string]
+    creation_datetime: DjangoDateTime
+    update_datetime: DjangoDateTime
+    session: int64
+    session_details: SessionSerializable
+    encounter: Option[int64]
+    connection: seq[QuoteConnectionSerializable]
+
 
 
 proc serializeQuoteRead*(connection: DbConn, entry: QuoteRead): QuoteSerializable =
-    result = entry
+    let session = connection.getEntryById(entry.session_id.id, SessionRead)
+    let quoteConnections = connection.getManyFromOne(entry, QuoteConnectionRead)
+
+    result = QuoteSerializable(
+        pk: entry.id,
+        quote: entry.quote,
+        description: entry.description,
+        creation_datetime: entry.creation_datetime,
+        update_datetime: entry.update_datetime,
+        session: entry.session_id.id,
+        session_details: connection.serializeSessionRead(session),
+        encounter: entry.encounter_id.map(enc => enc.id),
+        connection: quoteConnections.map(serializeQuoteConnectionRead)
+    )
 
 proc serializeQuote*(connection: DbConn, entry: Quote): QuoteSerializable =
     let fullEntry = connection.getEntryById(entry.id, QuoteRead)
     result = connection.serializeQuoteRead(fullEntry)
 
-
-proc serializeQuoteConnectionRead*(connection: DbConn, entry: QuoteConnectionRead): QuoteConnectionSerializable =
-  result = entry
