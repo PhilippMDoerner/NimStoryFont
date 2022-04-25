997c67dbf9a96531b3eb5c378a03f7faef6b6630
PhilippMDoerner <philippmdoerner@web.de>
2022-04-25 20:00:48 +0200

    Added Timestampserialization

--- src/applications/sessionAudioTimestamp/timestampSerialization.nim
+++ src/applications/sessionAudioTimestamp/timestampSerialization.nim
@@ -1,13 +1,29 @@
 import timestampModel
 import norm/sqlite
 import ../genericArticleRepository
+import std/[options]
 
-type TimestampSerializable* = TimestampRead
+type TimestampSerializable* = object
+    pk: int64
+    name: string
+    time: int
+    encounter: Option[int64]
+    session_audio: int64
 
 proc serializeTimestampRead*(connection: DbConn, entry: TimestampRead): TimestampSerializable =
-    result = entry
+    result = TimestampSerializable(
+        pk: entry.id,
+        name: entry.name,
+        time: entry.time,
+        encounter: entry.encounter_id,
+        session_audio: entry.session_audio_id.id
+    )
 
 proc serializeTimestamp*(connection: DbConn, entry: Timestamp): TimestampSerializable =
-    let fullEntry = connection.getEntryById(entry.id, TimestampRead)
-    result = connection.serializeTimestampRead(fullEntry)
-
+    result = TimestampSerializable(
+        pk: entry.id,
+        name: entry.name,
+        time: entry.time,
+        encounter: entry.encounter_id,
+        session_audio: entry.session_audio_id
+    )
