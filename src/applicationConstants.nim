136bb6dbcba2c447529d1c8919beee14441c1ac1
Philipp Doerner <show>
2022-04-28 22:02:03 +0200

    Added id prefix const

--- src/applicationConstants.nim
+++ src/applicationConstants.nim
@@ -2,7 +2,7 @@ import prologue/middlewares/cors
 
 const MODEL_INIT_ID*: int64 = -1
 const NONE_STRING_LOWER*: string = "none"
-
+const ID_STRING_PREFIX*: string = "id_"
 
 
 # CSP_DEFAULT_SRC = ("'none'", )
