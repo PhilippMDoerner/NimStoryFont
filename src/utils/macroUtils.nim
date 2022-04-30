c649d8c2bdee38b7b2f0e8d7cfd3211cd070d797
PhilippMDoerner <philippmdoerner@web.de>
2022-04-30 20:54:22 +0200

    Added some automatic sorting to fetching lists

--- src/utils/macroUtils.nim
+++ src/utils/macroUtils.nim
@@ -4,6 +4,12 @@ import norm/model
 macro getField*(obj: Model, fieldName: static string): untyped =
   nnkDotExpr.newTree(obj, ident(fieldName))
 
+proc hasField*[T](obj: T, fieldName: static string): bool {.compileTime.} =
+  result = compiles(obj.getField(fieldName))
+
+proc hasField*[T](t: typedesc[T], fieldName: static string): bool {.compileTime.} =
+  result = compiles(T().getField(fieldName))
+
 macro unpackFromJoinModel*[T: Model](mySeq: seq[T], field: static string): untyped =
   ## A macro to "extract" a field of name `field` out of the model in `mySeq`, 
   ## creating a new seq of whatever type the field called `field` has.
