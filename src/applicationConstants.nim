9141d7ccb85ebfbd03f591e8f3086ce299b89b24
PhilippMDoerner <philippmdoerner@web.de>
2022-04-30 04:57:21 +0200

    added utility proc to truncate text

--- src/applicationConstants.nim
+++ src/applicationConstants.nim
@@ -3,7 +3,7 @@ import prologue/middlewares/cors
 const MODEL_INIT_ID*: int64 = -1
 const NONE_STRING*: string = "none"
 const ID_STRING_PREFIX*: string = "id_"
-
+const SHORT_DESCRIPTION_WORD_COUNT*: int = 30
 
 # CSP_DEFAULT_SRC = ("'none'", )
 # CSP_STYLE_SRC = ("'self'", 'unpkg.com', 'stackpath.bootstrapcdn.com', 'fonts.googleapis.com', "'unsafe-inline'", 'cdn.jsdelivr.net', 'cdnjs.cloudflare.com', 'rawcdn.githack.com', 'maxcdn.bootstrapcdn.com',)
--- src/utils/myStrutils.nim
+++ src/utils/myStrutils.nim
@@ -1,5 +1,17 @@
-import std/algorithm
+import std/[strutils, htmlparser, xmltree, algorithm]
+import ../applicationConstants
 
 proc reverseString*(s: string): string =
   for character in reversed(s):
     result.add(character)
+
+
+proc truncate*(text: string): string =
+  let cleanedString = text.parseHtml().innerText
+  let splitString = cleanedString.split(" ")
+  
+  if splitString.len() <= SHORT_DESCRIPTION_WORD_COUNT:
+    result = cleanedString
+  else:
+    result.add(splitString[0..SHORT_DESCRIPTION_WORD_COUNT-1].join(" "))
+    result.add("...")
