7be1a1ebfd63db6e8903a63a30a5b29159ef75d3
PhilippMDoerner <philippmdoerner@web.de>
2022-05-02 18:08:57 +0200

    Added a second compression middleware based on miniz

--- src/middleware/compressionMiddleware.nim
+++ src/middleware/compressionMiddleware.nim
@@ -1,4 +1,5 @@
 import miniz
+import zippy
 import prologue
 import std/[strformat, strutils]
 
@@ -15,5 +16,24 @@ proc responseCompressionMiddleware*(): HandlerAsync =
       if allowCompression:
         ctx.response.headers.add("Content-Encoding", "deflate")
         ctx.response.headers.add("X-org-size", $ctx.response.body.len) # just for me to see the difference
-        ctx.response.body = ctx.response.body.compress()
+        ctx.response.body = miniz.compress(ctx.response.body)
         ctx.response.headers.add("X-compressed-size", $ctx.response.body.len) # just for me to see the difference
+
+
+proc responseCompressionMiddleware2*(): HandlerAsync =
+  ## Compresses the response
+  result = proc(ctx: Context) {.async.} =
+    await switch(ctx)
+
+    if ctx.request.headers.hasKey("Accept-Encoding"):
+      let encodings: seq[string] = ctx.request.getHeaderOrDefault("Accept-Encoding")
+      assert(encodings.len() == 1, "The expected buggy behaviour is that the entires tring of Accept-Encoding is packaged as one value")
+      let allowCompression = encodings[0].contains("deflate")
+      
+      if allowCompression:
+        ctx.response.headers.add("Content-Encoding", "deflate")
+        ctx.response.headers.add("X-org-size", $ctx.response.body.len) # just for me to see the difference
+        ctx.response.body = compress(ctx.response.body, BestCompression, CompressedDataFormat.dfDeflate)
+        ctx.response.headers.add("X-compressed-size", $ctx.response.body.len) # just for me to see the difference
+
+#      compress("gzip'ed response body", BestSpeed, dfGzip),
