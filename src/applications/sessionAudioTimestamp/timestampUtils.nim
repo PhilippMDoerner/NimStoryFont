81b2ff97d484d45b4a2ab4e4cad1582b3f342847
Philipp Doerner <show>
2022-05-03 07:37:33 +0200

    Fixed borked permission checking

--- src/applications/sessionAudioTimestamp/timestampUtils.nim
+++ src/applications/sessionAudioTimestamp/timestampUtils.nim
@@ -5,6 +5,7 @@ import prologue
 import ../../utils/jwtContext
 import ../allUrlParams
 import ../authentication/authenticationUtils
+import std/[encodings, strutils]
 
 proc campaign_id*(entry: Timestamp): int64 =
   result = getEntryById(entry.session_audio_id, TimestampSessionAudio).session_id.campaign_id.id
@@ -15,6 +16,6 @@ proc campaign_id*(entry: TimestampRead): int64 =
 
 proc checkReadTimestampListPermission*(ctx: JWTContext, entries: seq[TimestampRead]) =
   let campaignName = ctx.getPathParamsOption(CAMPAIGN_NAME_PARAM).get()
-  let hasCampaignMembership = ctx.tokenData.campaignMemberships.hasKey(campaignName)
+  let hasCampaignMembership = ctx.tokenData.campaignMemberships.hasKey(campaignName.toLower())
   if not hasCampaignMembership:
     raise newException(CampaignPermissionError, "You must be invited to a campaign to read its entries")
