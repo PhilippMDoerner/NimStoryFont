eddb6e3ae85b40453d3ff0a6d7bbc2441be8a7d7
PhilippMDoerner <philippmdoerner@web.de>
2022-05-16 18:54:43 +0200

    Added url endpoint for marker type overview and adjusted url

--- src/applications/mapMarkerType/markerTypeRoutes.nim
+++ src/applications/mapMarkerType/markerTypeRoutes.nim
@@ -10,28 +10,28 @@ import ../genericArticleControllers
 
 proc addMarkerTypeRoutes*(app: Prologue) =
     app.addRoute(
-        re fmt"/markerType/{CAMPAIGN_NAME_PATTERN}/",
+        re fmt"/markertype/{CAMPAIGN_NAME_PATTERN}/",
         handler = createCreateHandler[CreateParams, MarkerType, MarkerTypeSerializable](checkAdminPermission, createArticle, serializeMarkerType),
         httpMethod = HttpPost,
         middlewares = @[loginMiddleware()]
     )
 
     app.addRoute(
-        re fmt"/markerType/pk/{ID_PATTERN}/", 
+        re fmt"/markertype/pk/{ID_PATTERN}/", 
         handler = createDeleteHandler[DeleteParams, MarkerType](readArticleById, checkAdminPermission, deleteArticle),
         httpMethod = HttpDelete,
         middlewares = @[loginMiddleware()]
     )
 
     app.addRoute(
-        re fmt"/markerType/pk/{ID_PATTERN}/", 
+        re fmt"/markertype/pk/{ID_PATTERN}/", 
         handler = createUpdateHandler[UpdateParams, MarkerType, MarkerTypeSerializable](readArticleById, checkAdminPermission, updateArticle, serializeMarkerType),
         httpMethod = HttpPut,
         middlewares = @[loginMiddleware()]
     )
 
     app.addRoute(
-        re fmt"/markerType/", 
+        re fmt"/markertype/", 
         handler = createReadListHandler(
             readListProc = getMarkerTypes, 
             checkPermission = checkReadMarkerTypeListPermission, 
@@ -42,3 +42,13 @@ proc addMarkerTypeRoutes*(app: Prologue) =
     )
 
    
+    app.addRoute(
+        re fmt"/markertype/{CAMPAIGN_NAME_PATTERN}/overview", 
+        handler = createReadListHandler(
+            readListProc = getMarkerTypes, 
+            checkPermission = checkReadMarkerTypeListPermission, 
+            serialize = serializeMarkerType
+        ),  
+        httpMethod = HttpGet,
+        middlewares = @[loginMiddleware()]
+    )
