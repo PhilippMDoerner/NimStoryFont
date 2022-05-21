7d002f0a8f63d405fabf6ba5e318b07289622965
Philipp Doerner <show>
2022-05-21 09:09:01 +0200

    Added create/delete routes for spellconnections

--- src/applications/spell/spellUtils.nim
+++ src/applications/spell/spellUtils.nim
@@ -1,4 +1,17 @@
 import spellModel
+import std/[strformat, options]
+import ../../utils/jwtContext
+import ../authentication/authenticationUtils
+import ../genericArticleRepository
+
 
 proc `$`*(model: Spell | SpellRead): string = 
     model.name
+
+proc campaign_id*(entry: SpellConnection): int64 =
+    result = getEntryById(entry.spell_id, Spell).campaign_id
+
+proc campaign_id*(entry: SpellConnectionRead): int64 = entry.spell_id.campaign_id
+
+proc checkSpellConnectionCreatePermission*(ctx: JWTContext, entry: SpellConnection) =
+  checkCreatePermission(ctx, entry)
