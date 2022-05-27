04c9398bc8ae6e68e275f3d08d7d79cba4119c66
Philipp Doerner <show>
2022-05-27 22:10:14 +0200

    Added playercharacters endpoint

--- src/applications/character/characterRepository.nim
+++ src/applications/character/characterRepository.nim
@@ -3,13 +3,20 @@ import characterModel
 import ../../utils/djangoDateTime/[normConversion]
 import std/[strformat]
 
-proc getNonPlayerCharacters*(connection: DbConn, campaignName: string): seq[CharacterOverview] =
+proc getCharacterSet(connection: DbConn, campaignName: string, isPlayerCharacter: bool): seq[CharacterOverview] =
   result = @[newModel(CharacterOverview)]
 
   const condition = fmt"""
     campaign_id.name LIKE ? 
-    AND {CharacterOverview.table()}.player_character IS FALSE
+    AND {CharacterOverview.table()}.player_character IS ?
     ORDER BY {CharacterOverview.table()}.name ASC
   """
 
-  connection.select(result, condition, campaignName.dbValue())
+  connection.select(result, condition, campaignName.dbValue(), isPlayerCharacter.dbValue())
+
+proc getNonPlayerCharacters*(connection: DbConn, campaignName: string): seq[CharacterOverview] =
+  result = connection.getCharacterSet(campaignName, false)
+
+proc getPlayerCharacters*(connection: DbConn, campaignName: string): seq[CharacterOverview] =
+  result = connection.getCharacterSet(campaignName, true)
+
