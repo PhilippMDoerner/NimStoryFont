16aec56f66a4d0be3bf1cb28a92aed9bced3ad47
Philipp Doerner <show>
2022-06-11 21:06:50 +0200

    Further improvements

--- retryDocker.sh
+++ retryDocker.sh
@@ -3,5 +3,5 @@ sudo docker container stop test
 sudo docker container rm test
 sudo docker build -t nsi .
 echo "Run new container"
-# sudo docker run -it -p 80:80 -p 443:443 -p 8080:8080 --name test nsi
-sudo docker run -d -p 80:80 -p 443:443 -p 8080:8080 --name test nsi
+sudo docker run -it -p 80:80 -p 443:443 -v /etc/letsencrypt/live/www.aldrune.com:/certificates -v /home/philipp/dev/nimstoryfont/:/database -v /home/philipp/dev/nimstoryfont/media:/imagemedia -v /home/philipp/dev/nimstoryfont/media/session_audio:/audiomedia --name test nsi
+# sudo docker run -d -p 80:80 -p 443:443 -v /etc/ --name test nsi
