7b5bc105e56c7ea35f2c8f986d1be9464e497998
Philipp Doerner <show>
2022-06-11 16:20:04 +0200

    updated

--- retryDocker.sh
+++ retryDocker.sh
@@ -3,4 +3,4 @@ sudo docker container stop test
 sudo docker container rm test
 sudo docker build -t nsi .
 echo "Run new container"
-sudo docker run -it -p 80:80 -p 443:443 --name test nsi
+sudo docker run -it -p 80:80 -p 443:443 -p 8080:8080 --name test nsi
