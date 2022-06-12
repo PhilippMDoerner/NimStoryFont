c191723aaff62ea8809786ead91dbffafb190209
PhilippMDoerner <philippmdoerner@web.de>
2022-06-12 14:23:51 +0200

    Adjust volumes for audio streaming and stuff

--- retryDocker.sh
+++ retryDocker.sh
@@ -3,5 +3,4 @@ sudo docker container stop test
 sudo docker container rm test
 sudo docker build -t nsi .
 echo "Run new container"
-sudo docker run -it -p 80:80 -p 443:443 -v /etc/letsencrypt/live/www.aldrune.com:/certificates -v /home/philipp/dev/nimstoryfont/:/database -v /home/philipp/dev/nimstoryfont/media:/imagemedia -v /home/philipp/dev/nimstoryfont/media/session_audio:/audiomedia --name test nsi
-# sudo docker run -d -p 80:80 -p 443:443 -v /etc/ --name test nsi
+sudo docker run -it -p 80:80 -p 443:443 -v /etc/letsencrypt/live/www.aldrune.com:/certificates -v /home/philipp/dev/nimstoryfont/:/database -v /home/philipp/dev/nimstoryfont/media:/imagemedia -v /home/philipp/Music/session_audio_backup:/audiomedia --name test nsi
