ee071a0b3f9e8d80d2177961ae199b7678c4c6ae
PhilippMDoerner <philippmdoerner@web.de>
2022-06-12 16:03:23 +0200

    Added frontend into container

--- retryDocker.sh
+++ retryDocker.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 sudo docker container stop test
 sudo docker container rm test
+
+cp -r /home/philipp/dev/aldrunewikifrontend/dist .
+
 sudo docker build -t nsi .
+
+rm -r /home/philipp/dev/nimstoryfont/dist
+
 echo "Run new container"
 sudo docker run -it -p 80:80 -p 443:443 -v /etc/letsencrypt/live/www.aldrune.com:/certificates -v /home/philipp/dev/nimstoryfont/:/database -v /home/philipp/dev/nimstoryfont/media:/imagemedia -v /home/philipp/Music/session_audio_backup:/audiomedia --name test nsi
