dbba1206ef70bf30d9c4f19ee804c44d89b30d7c
Philipp Doerner <show>
2022-06-14 18:36:20 +0200

    Added a test docker thing

---
+++ testRebuildAndRunDocker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo docker container stop test
+sudo docker container rm test
+
+nim c -f --threads:on ./src/nimstoryfont.nim
+
+sudo docker build -t nsi .
+
+echo "Run new container"
+sudo docker run -it -p 80:80 -p 443:443 -v /etc/letsencrypt/live/www.aldrune.com:/certificates -v $HOME/dev/nimstoryfont/:/database -v $HOME/dev/nimstoryfont/media:/imagemedia -v $HOME/Music/session_audio_backup:/audiomedia -v $HOME/dev/aldrunewikifrontend/dist/frontend:/frontend -v /tmp:/var/log/nginx --name test nsi
