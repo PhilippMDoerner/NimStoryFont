446464428876fc28623317f754505eb774e9355d
Philipp Doerner <show>
2022-06-14 22:10:30 +0200

    Ensured that the compiled nim file gets moved to where the built one is

--- testRebuildAndRunDocker.sh
+++ testRebuildAndRunDocker.sh
@@ -3,8 +3,9 @@ sudo docker container stop test
 sudo docker container rm test
 
 nim c -f -d:normDebug -d:enableTinyPoolLogging --threads:on ./src/nimstoryfont.nim
+cp ./src/nimstoryfont ./nimstoryfont
 
 sudo docker build -t nsi .
 
 echo "Run new container"
-sudo docker run -it -p 80:80 -p 443:443 -v /etc/letsencrypt/live/www.aldrune.com:/certificates -v $HOME/dev/nimstoryfont/:/database -v $HOME/dev/nimstoryfont/media:/imagemedia -v $HOME/Music/session_audio_backup:/audiomedia -v $HOME/dev/aldrunewikifrontend/dist/frontend:/frontend -v /tmp:/var/log/nginx --name test nsi
+sudo docker run -it -p 80:80 -p 443:443 -p 8080:8080 -v /etc/letsencrypt/live/www.aldrune.com:/certificates -v $HOME/dev/nimstoryfont/:/database -v $HOME/dev/nimstoryfont/media:/imagemedia -v $HOME/Music/session_audio_backup:/audiomedia -v $HOME/dev/aldrunewikifrontend/dist/frontend:/frontend -v /tmp:/var/log/nginx --name test nsi
