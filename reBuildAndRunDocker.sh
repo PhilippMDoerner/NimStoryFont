4aaa6b66eac4e176ad8e469aa68507d0e7ffb02e
PhilippMDoerner <philippmdoerner@web.de>
2022-06-16 22:13:29 +0200

    Added mounting for extended directory

--- reBuildAndRunDocker.sh
+++ reBuildAndRunDocker.sh
@@ -7,4 +7,4 @@ nimble release
 sudo docker build -t nsi .
 
 echo "Run new container"
-sudo docker run -it -p 80:80 -p 443:443 -v /etc/letsencrypt/live/www.aldrune.com:/certificates -v $HOME/dev/nimstoryfont/:/database -v $HOME/dev/nimstoryfont/media:/imagemedia -v $HOME/Music/session_audio_backup:/audiomedia -v $HOME/dev/aldrunewikifrontend/dist/frontend:/frontend -v /tmp:/var/log/nginx --name test nsi
+sudo docker run -it -p 80:80 -p 443:443 -v /etc/letsencrypt/live/www.aldrune.com:/certificates -v $HOME/dev/nimstoryfont/:/database -v $HOME/dev/nimstoryfont/media:/imagemedia -v $HOME/Music/session_audio_backup:/audiomedia -v $HOME/Music/session_audio_backup/extended:/audiomedia/extended -v $HOME/dev/aldrunewikifrontend/dist/frontend:/frontend -v /tmp:/var/log/nginx --name test nsi
