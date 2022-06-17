da0b53fd59f982928476d8c4ac3e06d76ced0bdf
Philipp Doerner <show>
2022-06-17 09:56:41 +0200

    updated tooling

--- reBuildAndRunDocker.sh
+++ reBuildAndRunDocker.sh
@@ -5,6 +5,8 @@ sudo docker container rm test
 nimble release
 
 sudo docker build -t nsi .
+sudo docker save -o image.tar nsi
 
 echo "Run new container"
 sudo docker run -it -p 80:80 -p 443:443 -v /etc/letsencrypt/live/www.aldrune.com:/certificates -v $HOME/dev/nimstoryfont/:/database -v $HOME/dev/nimstoryfont/media:/imagemedia -v $HOME/Music/session_audio_backup:/audiomedia -v $HOME/Music/session_audio_backup/extended:/audiomedia/extended -v $HOME/dev/aldrunewikifrontend/dist/frontend:/frontend -v /tmp:/var/log/nginx --name test nsi
+
