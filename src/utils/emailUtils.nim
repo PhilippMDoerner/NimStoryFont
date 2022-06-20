e5a8c3e6e3497f88eda5cffca7b08c66e4823fe9
Philipp Doerner <show>
2022-06-20 12:53:06 +0200

    Moved over some calls extracting settings to getSettings proc

--- src/utils/emailUtils.nim
+++ src/utils/emailUtils.nim
@@ -1,5 +1,6 @@
 import std/smtp
 import prologue
+import ../applicationSettings
 
 type EmailDTO* = object
   subject*: string
@@ -14,11 +15,12 @@ proc sendSystemEmail*(subject: string, body: string, target: string, settings: S
   var msg = createMessage(subject, body, @[target])
   let smtpConn = newSmtp(useSsl = true, debug=true)
   
-  let smtpServerName = settings.getOrDefault("smtp").getStr()
+  let smtpServerName = settings.getSetting(SettingName.snSmtpName).getStr()
+  let smtpPort: Port = Port settings.getSetting(SettingName.snSmtpPort).getInt()
   smtpConn.connect(smtpServerName, Port 465)
 
-  let emailUserName = settings.getOrDefault("emailName").getStr()
-  let emailPassword = settings.getOrDefault("emailPassword").getStr()
+  let emailUserName = settings.getSetting(SettingName.snEmailName).getStr()
+  let emailPassword = settings.getSetting(SettingName.snEmailPassword).getStr()
   smtpConn.auth(emailUserName, emailPassword)
 
   smtpConn.sendmail(emailUserName, @[target], $msg)
