478abf169ed299aa7d2c65b7159ee0077ca622c0
PhilippMDoerner <philippmdoerner@web.de>
2022-06-20 21:02:16 +0200

    Differentiated prod and bug images

--- reBuildAndRunDocker.sh
+++ reBuildAndRunDocker.sh
@@ -4,9 +4,9 @@ sudo docker container rm test
 
 nimble release
 
-sudo docker build -t nsi .
-sudo docker save -o image.tar nsi
+sudo docker build -t nsi_prod .
+sudo docker save -o image.tar nsi_prod
 
 echo "Run new container"
-sudo docker run -it -p 80:80 -p 443:443 -v /etc/letsencrypt/live/www.aldrune.com:/certificates -v $HOME/dev/nimstoryfont/:/database -v $HOME/dev/nimstoryfont/media:/imagemedia -v $HOME/Music/session_audio_backup:/audiomedia -v $HOME/Music/session_audio_backup/extended:/audiomedia/extended -v $HOME/dev/aldrunewikifrontend/dist/frontend:/frontend -v /tmp:/var/log/nginx --name test nsi
+sudo docker run -it -p 80:80 -p 443:443 -v /etc/letsencrypt/live/www.aldrune.com:/certificates -v $HOME/dev/nimstoryfont/:/database -v $HOME/dev/nimstoryfont/media:/imagemedia -v $HOME/Music/session_audio_backup:/audiomedia -v $HOME/Music/session_audio_backup/extended:/audiomedia/extended -v $HOME/dev/aldrunewikifrontend/dist/frontend:/frontend -v /tmp:/var/log/nginx --name test nsi_prod
 
--- testRebuildAndRunDocker.sh
+++ testRebuildAndRunDocker.sh
@@ -5,7 +5,7 @@ sudo docker container rm test
 nim c -f -d:normDebug -d:ssl --stackTrace:on --lineTrace:on -d:enableTinyPoolLogging --threads:on ./src/nimstoryfont.nim
 cp ./src/nimstoryfont ./nimstoryfont
 
-sudo docker build -t nsi .
+sudo docker build -t nsi_debug .
 
 echo "Run new container"
-sudo docker run -it -p 80:80 -p 443:443 -p 8080:8080 -v /etc/letsencrypt/live/www.aldrune.com:/certificates -v $HOME/dev/nimstoryfont/:/database -v $HOME/dev/nimstoryfont/media:/imagemedia -v $HOME/Music/session_audio_backup:/audiomedia -v $HOME/Music/session_audio_backup/extended:/audiomedia/extended -v $HOME/dev/aldrunewikifrontend/dist/frontend:/frontend -v /tmp:/var/log/nginx --name test nsi
+sudo docker run -it -p 80:80 -p 443:443 -p 8080:8080 -v /etc/letsencrypt/live/www.aldrune.com:/certificates -v $HOME/dev/nimstoryfont/:/database -v $HOME/dev/nimstoryfont/media:/imagemedia -v $HOME/Music/session_audio_backup:/audiomedia -v $HOME/Music/session_audio_backup/extended:/audiomedia/extended -v $HOME/dev/aldrunewikifrontend/dist/frontend:/frontend -v /tmp:/var/log/nginx --name test nsi_debug
