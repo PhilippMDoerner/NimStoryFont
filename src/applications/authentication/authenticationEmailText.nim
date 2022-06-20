130cc26385cf036b7d880efd6c493b1f0318db7a
PhilippMDoerner <philippmdoerner@web.de>
2022-06-20 20:58:29 +0200

    Added some mail text

--- src/applications/authentication/authenticationEmailText.nim
+++ src/applications/authentication/authenticationEmailText.nim
@@ -4,8 +4,8 @@ type EmailPlaceholder = enum
   epPassword = "{PASSWORD_PLACEHOLDER}"
   epUsername = "{USERNAME}"
 
-const PASSWORD_RESET_SUBJECT* = "Password reset of '{USERNAME}' account on nimstoryfont"
-const PASSWORD_RESET_BODY* = "Password reset was carried out as requested.<br> Your new password is: {PASSWORD_PLACEHOLDER}<br><br> Click <a href='https://wwww.aldrune.com/wiki2'> here </a> to get to the login page"
+const PASSWORD_RESET_SUBJECT* = "{USERNAME}, your password on nimstoryfont was reset!"
+const PASSWORD_RESET_BODY* = "Password reset was carried out as requested.<br> Your new password is: {PASSWORD_PLACEHOLDER}. Please swap it out as soon as possible. <br><br> Click <a href='https://wwww.aldrune.com/wiki2'> here </a> to get to the login page"
 
 proc fillPlaceholder(coreString: string, placeholder: EmailPlaceholder, value: string): string =
   result = coreString.replace($placeholder, value)
