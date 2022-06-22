82ed230593a12c9b5d8916aae774ab99189a1fce
PhilippMDoerner <philippmdoerner@web.de>
2022-06-22 20:53:56 +0200

    Added convenience thing

--- reBuildAndRunDocker.sh
+++ reBuildAndRunDocker.sh
@@ -6,6 +6,7 @@ nimble release
 
 sudo docker build -t nsi_prod .
 sudo docker save -o image.tar nsi_prod
+sudo chmod 777 image.tar
 
 echo "Run new container"
 sudo docker run -it -p 80:80 -p 443:443 -v /etc/nginx/certificates:/certificates -v $HOME/dev/nimstoryfont/:/database -v $HOME/dev/nimstoryfont/media:/imagemedia -v $HOME/Music/session_audio_backup:/audiomedia -v $HOME/Music/session_audio_backup/extended:/audiomedia/extended -v $HOME/dev/aldrunewikifrontend/dist/frontend:/frontend -v /tmp:/var/log/nginx --name test nsi_prod
