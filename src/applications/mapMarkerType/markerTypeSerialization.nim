693899bf51d421d1003252fd28d9c54a20173832
Philipp Doerner <show>
2022-06-26 11:05:27 +0200

    #67 added map/marker many serialization

--- src/applications/mapMarkerType/markerTypeSerialization.nim
+++ src/applications/mapMarkerType/markerTypeSerialization.nim
@@ -6,4 +6,4 @@ type MarkerTypeSerializable* = MarkerType
 
 proc serializeMarkerType*(connection: DbConn, entry: MarkerType): MarkerTypeSerializable = entry
 
-
+proc serializeMarkerTypes*(connection: DbConn, entries: seq[MarkerType]): seq[MarkerTypeSerializable] = entries
