5ecfff04af2a41c3e53e07a7b8eab1b75770bcf6
Philipp Doerner <show>
2022-06-26 11:12:10 +0200

    #67 added timestamp many serialization

--- src/applications/sessionAudioTimestamp/timestampRoutes.nim
+++ src/applications/sessionAudioTimestamp/timestampRoutes.nim
@@ -26,7 +26,7 @@ proc addTimestampRoutes*(app: Prologue) =
 
     app.addRoute(
         re fmt"/timestamp/{CAMPAIGN_NAME_PATTERN}/{SESSION_IS_MAIN_SESSION_PATTERN}/{SESSION_NUMBER_PATTERN}/", 
-        handler = createReadListHandler(getTimestampsForSessionAudio, checkReadTimestampListPermission, serializeTimestampRead),  
+        handler = createReadListHandler(getTimestampsForSessionAudio, checkReadTimestampListPermission, serializeTimestampReads),  
         httpMethod = HttpGet,
         middlewares = @[loginMiddleware()]
     )
