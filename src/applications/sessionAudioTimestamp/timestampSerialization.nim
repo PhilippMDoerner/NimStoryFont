dcb6144fa0c6b9fea24fec113c31b91134352ef0
PhilippMDoerner <philippmdoerner@web.de>
2022-06-26 11:12:10 +0200

    #67 added timestamp many serialization

--- src/applications/sessionAudioTimestamp/timestampSerialization.nim
+++ src/applications/sessionAudioTimestamp/timestampSerialization.nim
@@ -1,7 +1,7 @@
 import timestampModel
 import norm/sqlite
 import ../genericArticleRepository
-import std/[options]
+import std/[options, sugar, sequtils]
 
 type TimestampSerializable* = object
     pk: int64
@@ -10,20 +10,20 @@ type TimestampSerializable* = object
     encounter: Option[int64]
     session_audio: int64
 
-proc serializeTimestampRead*(connection: DbConn, entry: TimestampRead): TimestampSerializable =
+proc serialize(entry: TimestampRead | Timestamp, sessionAudioId: int64): TimestampSerializable = 
     result = TimestampSerializable(
         pk: entry.id,
         name: entry.name,
         time: entry.time,
         encounter: entry.encounter_id,
-        session_audio: entry.session_audio_id.id
+        session_audio: sessionAudioId
     )
 
+proc serializeTimestampRead*(connection: DbConn, entry: TimestampRead): TimestampSerializable =
+    result = serialize(entry, entry.session_audio_id.id)
+
 proc serializeTimestamp*(connection: DbConn, entry: Timestamp): TimestampSerializable =
-    result = TimestampSerializable(
-        pk: entry.id,
-        name: entry.name,
-        time: entry.time,
-        encounter: entry.encounter_id,
-        session_audio: entry.session_audio_id
-    )
+    result = serialize(entry, entry.session_audio_id)
+
+proc serializeTimestampReads*(connection: DbConn, entries: seq[TimestampRead]): seq[TimestampSerializable] =
+    result = entries.map(entry => connection.serializeTimestampRead(entry))
