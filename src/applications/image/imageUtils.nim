4599e983ad344b317334fc94c49de5f8bf94ef4c
PhilippMDoerner <philippmdoerner@web.de>
2022-06-29 22:56:16 +0200

    Fixed really dumb mistake

--- src/applications/image/imageUtils.nim
+++ src/applications/image/imageUtils.nim
@@ -15,11 +15,11 @@ proc campaign_id*(entry: Image): int64 =
     result = getEntryById(entry.character_article_id.get(), ImageCharacter).campaign_id
   elif(entry.creature_article_id.isSome()):
     result = getEntryById(entry.creature_article_id.get(), ImageCreature).campaign_id
-  elif(entry.creature_article_id.isSome()):
+  elif(entry.item_article_id.isSome()):
     result = getEntryById(entry.item_article_id.get(), ImageItem).campaign_id
-  elif(entry.creature_article_id.isSome()):
+  elif(entry.location_article_id.isSome()):
     result = getEntryById(entry.location_article_id.get(), ImageLocation).campaign_id
-  elif(entry.creature_article_id.isSome()):
+  elif(entry.organization_article_id.isSome()):
     result = getEntryById(entry.organization_article_id.get(), ImageOrganization).campaign_id
   else:
     raise newException(InvalidImageError, fmt"An impossible image occurred that wasn't associated with any article type. Image Id was {entry.id}")
