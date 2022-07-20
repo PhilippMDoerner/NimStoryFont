bca66891a8545fbdf5706a0688f2acf17bc4b12b
Philipp Doerner <show>
2022-07-20 18:32:43 +0200

    Fixed access permission issue

--- src/applications/sessionAudioTimestamp/timestampUtils.nim
+++ src/applications/sessionAudioTimestamp/timestampUtils.nim
@@ -15,6 +15,6 @@ proc campaign_id*(entry: TimestampRead): int64 =
 
 proc checkReadTimestampListPermission*(ctx: JWTContext, entries: seq[TimestampRead]) =
   let campaignName = ctx.getPathParamsOption(CAMPAIGN_NAME_PARAM).get()
-  let hasCampaignMembership = ctx.tokenData.campaignMemberships.hasKey(campaignName.toLower())
+  let hasCampaignMembership = ctx.tokenData.campaignMemberships.hasKey(campaignName) or ctx.tokenData.campaignMemberships.hasKey(campaignName.toLower())
   if not hasCampaignMembership:
     raise newException(CampaignPermissionError, "You must be invited to a campaign to read its entries")
