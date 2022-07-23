ec17bf3fa8e4c55bfebc74c18d210fa46246e11e
Philipp Doerner <show>
2022-07-23 11:14:34 +0200

    Added convenience stuff to scripts

--- pushToProd.sh
+++ pushToProd.sh
@@ -3,5 +3,7 @@ sudo docker build -t nsi_prod .
 
 sudo docker save -o image.tar nsi_prod
 sudo chmod 777 image.tar
+bash ./disableDev.sh
 
 scp image.tar isofruit@aldrune.com:~/image.tar
+ssh isofruit@aldrune.com 'bash startNimstoryfont.sh'
