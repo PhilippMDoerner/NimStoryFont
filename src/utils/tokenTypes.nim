3e785bd9ea1ce9691b727baf1ce24d2bf107914d
PhilippMDoerner <philippmdoerner@web.de>
2022-07-31 13:06:46 +0200

    Slight beautification

--- src/utils/tokenTypes.nim
+++ src/utils/tokenTypes.nim
@@ -9,7 +9,9 @@ type TokenType* {.pure.} = enum
   refresh = "refresh"
 
 type CampaignAccessLevel* = enum
-  GUEST = "guest", MEMBER = "member", ADMIN = "admin"
+  GUEST = "guest", 
+  MEMBER = "member", 
+  ADMIN = "admin"
 
 type CampaignIdType* = enum
     citString,
@@ -19,8 +21,18 @@ type CampaignIdentifier* = object
     case kind*: CampaignIdType
     of citString: campaignName*: string
     of citInt: id*: int64
-proc newCampaignIdentifier*(id: int64): CampaignIdentifier = CampaignIdentifier(kind: CampaignIdType.citInt, id: id)
-proc newCampaignIdentifier*(campaignName: string): CampaignIdentifier = CampaignIdentifier(kind: CampaignIdType.citString, campaignName: campaignName)
+
+type CampaignMemberships* = TableRef[CampaignIdentifier, CampaignAccessLevel]
+
+
+
+## CampaignIdentifier Utils
+proc newCampaignIdentifier*(id: int64): CampaignIdentifier = 
+  result = CampaignIdentifier(kind: CampaignIdType.citInt, id: id)
+
+proc newCampaignIdentifier*(campaignName: string): CampaignIdentifier = 
+  result = CampaignIdentifier(kind: CampaignIdType.citString, campaignName: campaignName)
+
 proc `==`*(id1, id2: CampaignIdentifier): bool =
     if id1.kind != id2.kind:
         return false
@@ -28,31 +40,11 @@ proc `==`*(id1, id2: CampaignIdentifier): bool =
     result = case id1.kind:
     of CampaignIdType.citString: id1.campaignName == id2.campaignName
     of CampaignIdType.citInt: id1.id == id2.id 
+
 proc `$`*(id: CampaignIdentifier): string =
   result = case id.kind:
   of citString: id.campaignName
   of citInt: fmt"{ID_STRING_PREFIX}{$id.id}"
-
-
-type CampaignMemberships* = TableRef[CampaignIdentifier, CampaignAccessLevel]
-proc newMembershipTable*(): CampaignMemberships = newTable[CampaignIdentifier, CampaignAccessLevel]()
-proc hasKey*(table: CampaignMemberships, key: int64 | string): bool = table.hasKey(newCampaignIdentifier(key))
-proc `[]`*(table: CampaignMemberships, identifier: int64 | string): CampaignAccessLevel =
-  result = table[newCampaignIdentifier(identifier)]
-proc `[]=`*(table: var CampaignMemberships, identifier: int64, value: CampaignAccessLevel) =
-  let id = newCampaignIdentifier(identifier)
-  table[id] = value
-
-proc `[]=`*(table: var CampaignMemberships, identifier: string, value: CampaignAccessLevel) =
-  let isIntIdentifier = identifier.startsWith(ID_STRING_PREFIX)
-  if isIntIdentifier:
-    var idCopy = identifier
-    idCopy.removePrefix(ID_STRING_PREFIX)
-    table[newCampaignIdentifier(idCopy.parseInt().int64)] = value
-
-  else: 
-    table[newCampaignIdentifier(identifier)] = value
-
 
 proc dumpHook*(s: var string, identifier: CampaignIdentifier) =
   ## Implements serializing the identifier to JSON strings for jsony
@@ -73,12 +65,39 @@ proc jsonFieldName*(identifier: CampaignIdentifier): string =
   of CampaignIdType.citInt:
     result = fmt"{ID_STRING_PREFIX}{identifier.id}"
 
+
+## CampaignMemberships utils
+proc newMembershipTable*(): CampaignMemberships = 
+  result = newTable[CampaignIdentifier, CampaignAccessLevel]()
+
+proc hasKey*(table: CampaignMemberships, key: int64 | string): bool = 
+  result = table.hasKey(newCampaignIdentifier(key))
+
+proc `[]`*(table: CampaignMemberships, identifier: int64 | string): CampaignAccessLevel =
+  result = table[newCampaignIdentifier(identifier)]
+
+proc `[]=`*(table: var CampaignMemberships, identifier: int64, value: CampaignAccessLevel) =
+  let id = newCampaignIdentifier(identifier)
+  table[id] = value
+
+proc `[]=`*(table: var CampaignMemberships, identifier: string, value: CampaignAccessLevel) =
+  let isIntIdentifier = identifier.startsWith(ID_STRING_PREFIX)
+  if isIntIdentifier:
+    var idCopy = identifier
+    idCopy.removePrefix(ID_STRING_PREFIX)
+    table[newCampaignIdentifier(idCopy.parseInt().int64)] = value
+
+  else: 
+    table[newCampaignIdentifier(identifier)] = value
+
 proc toJson*(memberships: CampaignMemberships): JsonNode =
   result = JsonNode(kind: JObject)
   for identifier, accessLevel in memberships.pairs:
     let fieldName = identifier.jsonFieldName()
     result.fields[fieldName] = JsonNode(kind: JString, str: $accessLevel)
 
+
+## TokenData
 type TokenData* {.defaults.} = object
     campaignMemberships*: CampaignMemberships = newMembershipTable()
     isAdmin*: bool = false
@@ -88,8 +107,7 @@ type TokenData* {.defaults.} = object
     jti*: string = ""
     exp*: int64 = -1
 implDefaults(TokenData)
+
 proc newTokenData*(): TokenData = initTokenData()
 
 proc isInitTokenData*(tokenData: TokenData): bool = tokenData.userId == MODEL_INIT_ID
-
-proc isSiteAdmin*(tokenData: TokenData): bool = tokenData.isAdmin or tokenData.isSuperUser
