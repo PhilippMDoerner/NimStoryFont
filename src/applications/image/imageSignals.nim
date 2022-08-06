794c61a5459294a78a6808fb755f249935de3df5
PhilippMDoerner <philippmdoerner@web.de>
2022-08-06 12:41:57 +0200

    Removed unwanted signal for webp, that has been given up on

--- src/applications/image/imageSignals.nim
+++ src/applications/image/imageSignals.nim
@@ -15,17 +15,3 @@ proc deleteImageFile(connection: DbConn, modelInstance: Image) =
 
 proc connectImageSignals*() =
   connect(SignalType.stPreDelete, Image, deleteImageFile)
-
-
-#TODO: Finish webp conversion
-# proc isWebPImage(imageFilepath: string): bool = imageFilepath.endsWith(".webp")
-# proc convertReceivedImageToWebP(connection: DbConn, modelInstance: Image) =
-#   let imageFilepath: string = modelInstance.image
-#   if isWebPImage(imageFilepath): #No conversion necessary
-#     return
-
-#   let convertedImageFilepath = convertToWebP(imageFilepath)
-#   modelInstance.image = convertedImageFilepath
-
-#connect(SignalType.stPreCreate, Image, convertReceivedImageToWebP)
-#connect(SignalType.stPreUpdate, Image, convertReceivedImageToWebP)
