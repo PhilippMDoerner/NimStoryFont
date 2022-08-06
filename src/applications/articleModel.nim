7664bc358377ad6c3c2ffe0b8dab12046943769f
PhilippMDoerner <philippmdoerner@web.de>
2022-08-06 10:11:17 +0200

    Fixed hard coded string usage ArticleTable enum

--- src/applications/articleModel.nim
+++ src/applications/articleModel.nim
@@ -11,20 +11,21 @@ import sessionaudio/sessionaudioModel
 import spell/spellModel
 import rules/ruleModel
 import mapMarker/markerModel
+import ../applicationSettings
 
 type ArticleTable* = enum #TODO: Replace this with applicationSettings constants once that can compile
-  atbCHARACTER = "wikientries_character"
-  atbCREATURE = "wikientries_creature"
-  atbDIARYENTRY = "wikientries_diaryentry"
-  atbENCOUNTER = "wikientries_encounter"
-  atbITEM = "wikientries_item"
-  atbLOCATION = "wikientries_location"
-  atbMAP = "map_map"
-  atbORGANIZATION = "wikientries_organization"
-  atbQUEST = "wikientries_quest"
-  atbSESSIONAUDIO = "fileserver_sessionaudio"
-  atbSPELL = "wikientries_spell"
-  atbRULE = "wikientries_rules"
+  atbCHARACTER = CHARACTER_TABLE
+  atbCREATURE = CREATURE_TABLE
+  atbDIARYENTRY = DIARYENTRY_TABLE
+  atbENCOUNTER = ENCOUNTER_TABLE
+  atbITEM = ITEM_TABLE
+  atbLOCATION = LOCATION_TABLE
+  atbMAP = MAP_TABLE
+  atbORGANIZATION = ORGANIZATION_TABLE
+  atbQUEST = QUEST_TABLE
+  atbSESSIONAUDIO = SESSIONAUDIO_TABLE
+  atbSPELL = SPELL_TABLE
+  atbRULE = RULES_TABLE
 
 type Article* = Character | Creature | DiaryEntry | Encounter | Item | Location | Map | MarkerMap | Organization | OrganizationRead | Quest | SessionAudio | Spell | Rule
 type ArticleType* = enum
