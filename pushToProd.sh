290a8c681c4ad02bea69b656eb18ecfc406e606c
PhilippMDoerner <philippmdoerner@web.de>
2022-09-17 23:29:28 +0200

    REFACTOR building scripts

--- pushToProd.sh
+++ pushToProd.sh
@@ -1,9 +1,15 @@
-nimble alpine
-sudo docker build -t nsi_prod .
+nimble alpine # Build Binary
+bash buildImages.sh # Build Images with binary
 
-sudo docker save -o image.tar nsi_prod
-sudo chmod 777 image.tar
+# Generate tar files from built images
+sudo docker save -o nginx_image.tar nimstoryfont-proxy 
+sudo docker save -o ns_image.tar nimstoryfont-nimstoryfont
+
+sudo chmod 777 nginx_image.tar
+sudo chmod 777 ns_image.tar
 bash ./disabledev.sh
 
-scp image.tar isofruit@aldrune.com:~/image.tar
+# Upload tar files
+scp nginx_image.tar isofruit@aldrune.com:~/nginx_image.tar
+scp ns_image.tar isofruit@aldrune.com:~/ns_image.tar
 #ssh isofruit@aldrune.com 'bash startNimstoryfont.sh'
