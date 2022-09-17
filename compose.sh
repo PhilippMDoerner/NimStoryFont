054a1f1b675c008435ce5ee0ff442f88029c8af2
PhilippMDoerner <philippmdoerner@web.de>
2022-09-17 22:14:43 +0200

    ADD ability to use docker compose

---
+++ compose.sh
@@ -0,0 +1,16 @@
+echo "Removing old containers"
+sudo docker container stop production_nginx
+sudo docker container rm production_nginx
+sudo docker container stop backend
+sudo docker container rm backend
+
+echo "Removing old images"
+sudo docker image rm nimstoryfont-proxy
+sudo docker image rm nimstoryfont-nimstoryfont
+
+echo "Creating new images"
+sudo docker build --file ./buildFiles/nginx/dockerfile --tag nimstoryfont-proxy ./buildFiles/nginx
+sudo docker build --file ./buildFiles/nimstoryfont/dockerfile --tag nimstoryfont-nimstoryfont ./buildFiles/nimstoryfont
+
+echo "Composing new images"
+sudo docker-compose up
