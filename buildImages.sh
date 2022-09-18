acad3e48113ca116d7d37b523fec858697e2b1b7
PhilippMDoerner <philippmdoerner@web.de>
2022-09-18 08:55:10 +0200

    REFACTOR to make use of bash variables

--- buildImages.sh
+++ buildImages.sh
@@ -1,13 +1,18 @@
+nginx_container=production_nginx
+nginx_image=nimstoryfont-proxy
+ns_container=nswebserver
+ns_image=nimstoryfont-nimstoryfont
+
 echo "Removing old containers"
-sudo docker container stop production_nginx
-sudo docker container rm production_nginx
-sudo docker container stop backend
-sudo docker container rm backend
+sudo docker container stop $nginx_container
+sudo docker container rm $nginx_container
+sudo docker container stop $ns_container
+sudo docker container rm $ns_container
 
 echo "Removing old images"
-sudo docker image rm nimstoryfont-proxy
-sudo docker image rm nimstoryfont-nimstoryfont
+sudo docker image rm $nginx_image
+sudo docker image rm $ns_image
 
 echo "Creating new images"
-sudo docker build --file ./buildFiles/nginx/dockerfile --tag nimstoryfont-proxy ./buildFiles/nginx
-sudo docker build --file ./buildFiles/nimstoryfont/dockerfile --tag nimstoryfont-nimstoryfont ./buildFiles/nimstoryfont
+sudo docker build --file ./buildFiles/nginx/dockerfile --tag $nginx_image ./buildFiles/nginx
+sudo docker build --file ./buildFiles/nimstoryfont/dockerfile --tag $ns_image ./buildFiles/nimstoryfont
