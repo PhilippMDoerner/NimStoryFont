24397dd4e7486fad8c4313536cc9af67c21ee098
Philipp Doerner <show>
2022-10-23 12:42:25 +0200

    FIX missing default values
    Signed-off-by: Philipp Doerner <philippmdoerner@web.de>

--- src/applications/user/userRequestParams.nim
+++ src/applications/user/userRequestParams.nim
@@ -4,5 +4,5 @@ import ../allUrlParams
 export allUrlParams
 
 type ReadUserByNameParams* {.defaults.} = object
-  username*: string
+  username*: string = ""
 implDefaults(ReadUserByNameParams, {DefaultFlag.defExported, DefaultFlag.defTypeConstr})
