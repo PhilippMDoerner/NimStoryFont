cc3e6ee015a73858889ef263feca506d53b4aa98
PhilippMDoerner <philippmdoerner@web.de>
2023-01-27 21:20:55 +0100

    Fix borked update to record
    You need to jsonify the article data, not just the article itself.

--- src/applications/search/searchService.nim
+++ src/applications/search/searchService.nim
@@ -7,27 +7,28 @@ import ./articleToStringUtils
 import ../articleModel
 import ../articleUtils
 
+proc getArticleTable[T: Article](model: typedesc[T]): ArticleTable {.compileTime.} =
+  const articleTable: string = T.table()[1..^2] # Removes parenthesis
+  result = parseEnum[ArticleTable](articleTable)
+
 proc deleteSearchEntry*(connection: DbConn, article: Article) =
   deleteSearchEntry(connection, article.getSearchGuid())
-
 
 proc updateSearchEntryContent*(connection: DbConn, article: Article) =
   let searchTitle: string = connection.getSearchTitle(article)
   let searchBody: string = connection.getSearchBody(article)
   let guid = article.getSearchGuid()
-  let articleJson = article.toJson()
-  updateSearchEntryContent(connection, guid, searchTitle, searchBody, articleJson)
+  const table: ArticleTable = getArticleTable(article.type)
+  let record: JsonNode = connection.getArticleData(table, article.id)
+  updateSearchEntryContent(connection, guid, searchTitle, searchBody, $record)
 
 
 proc addSearchEntry*(connection: DbConn, article: Article) =
   let searchTitle: string = connection.getSearchTitle(article)
   let searchBody: string = connection.getSearchBody(article)
-  var articleTable: string = article.type().table()
-  articleTable.removeSuffix('"')
-  articleTable.removePrefix('"')
-  let articleTableEnum = parseEnum[ArticleTable](articleTable)
-  let record: JsonNode = connection.getArticleData(articleTableEnum, article.id)
-  addSearchEntry(connection, searchTitle, searchBody, articleTable, article.id, $record, article.campaign_id)
+  const table: ArticleTable = getArticleTable(article.type)
+  let record: JsonNode = connection.getArticleData(table, article.id)
+  addSearchEntry(connection, searchTitle, searchBody, $table, article.id, $record, article.campaign_id)
 
 
 proc findArticles*(campaignName: string, searchText: string, searchLimit: int = 100): seq[SearchSerializable] =
