3fe716dc0cc62e93988d502b13904e18e8311b60
PhilippMDoerner <philippmdoerner@web.de>
2024-11-11 22:49:27 +0100

    Fix routing order

--- src/applications/search/searchRoutes.nim
+++ src/applications/search/searchRoutes.nim
@@ -6,6 +6,12 @@ import ../allUrlParams
 
 proc addSearchRoutes*(app: Prologue) =
     app.addRoute(
+        re fmt"/search/single/{ARTICLE_TYPE_PATTERN}/{ID_PATTERN}",
+        handler = searchControllers.findArticle,
+        httpMethod = HttpGet,
+        middlewares = @[loginMiddleware()]
+    )
+    app.addRoute(
         re fmt"/search/{CAMPAIGN_NAME_PATTERN}/{SEARCH_TEXT_PATTERN}",
         handler = searchControllers.findArticles,
         httpMethod = HttpGet,
@@ -14,13 +20,6 @@ proc addSearchRoutes*(app: Prologue) =
     app.addRoute(
         re fmt"/search/{CAMPAIGN_NAME_PATTERN}/{ARTICLE_TYPE_PATTERN}/{SEARCH_TEXT_PATTERN}",
         handler = searchControllers.findArticlesOfType,
-        httpMethod = HttpGet,
-        middlewares = @[loginMiddleware()]
-    )
-    
-    app.addRoute(
-        re fmt"/search/{ARTICLE_TYPE_PATTERN}/{ID_PATTERN}",
-        handler = searchControllers.findArticle,
         httpMethod = HttpGet,
         middlewares = @[loginMiddleware()]
     )
