33c3896b55d40b9245b648bd979470622083e434
PhilippMDoerner <philippmdoerner@web.de>
2025-04-13 12:21:00 +0200

    Update script

--- backend/downloadDb.sh
+++ backend/downloadDb.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Set the server details
-SERVER="isofruit@172.105.82.139"
-REMOTE_FILE="~/AldruneWiki/db.sqlite3"
-LOCAL_FILE="db.sqlite3"
+SERVER=isofruit@172.105.82.139
+REMOTE_FILE=~/AldruneWiki/db.sqlite3
+LOCAL_FILE=db.sqlite3
 
 # Use scp to download the file
 scp ${SERVER}:${REMOTE_FILE} ${LOCAL_FILE}
