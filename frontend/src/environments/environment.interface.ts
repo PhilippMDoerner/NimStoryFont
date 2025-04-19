545384a70c2f7524d20130a9d4d65cf07492d212
PhilippMDoerner <philippmdoerner@web.de>
2025-04-19 22:11:49 +0200

    Remove default title

--- frontend/src/environments/environment.interface.ts
+++ frontend/src/environments/environment.interface.ts
@@ -3,7 +3,6 @@ export type EnvironmentKind = 'PRODUCTION' | 'DEVELOPMENT';
 export interface Environment {
   kind: EnvironmentKind;
   apiUrl: string;
-  defaultTitle: string; //The default title shown in the tab heading in the browser
   frontendPrefix: string;
   configUrl: string;
 }
