d19499e3192012482cad5a343b614957bdd3dfcf
PhilippMDoerner <show>
2025-04-27 23:15:53 +0200

    Add migration to fix constraint

---
+++ backend/sql/V1.16__add_metadata_table2.sql
@@ -0,0 +1,9 @@
+DROP TABLE user_metadata;
+CREATE TABLE user_metadata (
+  id integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+  user_id integer NOT NULL REFERENCES auth_user (id) DEFERRABLE INITIALLY DEFERRED,
+  category varchar(200) NOT NULL,
+  name varchar(200) NOT NULL,
+  value varchar(200) NOT NULL,
+  CONSTRAINT unique_group_key UNIQUE (user_id, name, value)
+);
