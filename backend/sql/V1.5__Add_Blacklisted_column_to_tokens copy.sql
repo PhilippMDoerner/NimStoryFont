3f765cbfcfb49589c942e6b8c4cff520508ff8da
PhilippMDoerner <philippmdoerner@web.de>
2025-04-28 18:34:51 +0200

    Add db init

--- backend/sql/V1.5__Add_Blacklisted_column_to_tokens copy.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE authtoken_token;
-
-CREATE TABLE "authtoken_token" (
-    "key" varchar(40) NOT NULL PRIMARY KEY, 
-    "created" datetime NOT NULL, 
-    "user_id" integer NOT NULL REFERENCES "auth_user" ("id") DEFERRABLE INITIALLY DEFERRED, 
-    blacklisted bool NOT NULL DEFAULT FALSE,
-    tokenType varchar(10) NOT NULL DEFAULT "access"
-)
