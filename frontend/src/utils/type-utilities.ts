9060a6bac2cfaee39cd4352b8ea941910fc78c63
PhilippMDoerner <philippmdoerner@web.de>
2025-05-09 08:32:50 +0200

    Remove unused type

--- frontend/src/utils/type-utilities.ts
+++ frontend/src/utils/type-utilities.ts
@@ -4,10 +4,3 @@ export type ExpandRecursively<T> = T extends object
     ? { [K in keyof O]: ExpandRecursively<O[K]> }
     : never
   : T;
-
-// Utility type for getting the param type of a function... I think
-export type SomeVersionOfU2I<U> = (
-  U extends any ? (x: U) => any : never
-) extends (x: infer I) => any
-  ? I
-  : never;
