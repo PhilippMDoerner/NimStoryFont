9b62b097c7ceadb79fe0f20e408d656fd65f5754
PhilippMDoerner <philippmdoerner@web.de>
2025-05-09 08:02:07 +0200

    Enable conditionally autofocussing on render

--- frontend/src/app/_directives/focusOnRender.directive.ts
+++ frontend/src/app/_directives/focusOnRender.directive.ts
@@ -1,11 +1,16 @@
-import { afterNextRender, Directive, ElementRef } from '@angular/core';
+import { afterNextRender, Directive, ElementRef, input } from '@angular/core';
 
 @Directive({
   selector: '[focusOnRender]',
   standalone: true,
 })
 export class FocusOnRender {
+  focusOnRender = input<boolean>(true);
+
   constructor(public elementRef: ElementRef<HTMLElement>) {
-    afterNextRender(() => elementRef.nativeElement.focus());
+    afterNextRender(() => {
+      if (!this.focusOnRender()) return;
+      elementRef.nativeElement.focus();
+    });
   }
 }
