11a6d9b683ad447da726e0b7174a3ba9639c72d0
PhilippMDoerner <philippmdoerner@web.de>
2025-06-25 21:15:50 +0200

    Update environment to allow loading feature_config seamlessly

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -3,5 +3,5 @@ import { Environment } from './environment.interface';
 export const environment: Environment = {
   kind: 'PRODUCTION',
   apiUrl: `/wiki1/api`,
-  configUrl: `feature_config.json`,
+  configUrl: `/wiki1/feature_config.json`, //NOTE: This must be consistent with the Feature Config section in nginx.conf
 };
