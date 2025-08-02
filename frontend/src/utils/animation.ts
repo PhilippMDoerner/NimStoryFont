e3e2afbe0eb56aace8818eea73d04d7acc5b6524
PhilippMDoerner <philippmdoerner@web.de>
2025-08-02 20:01:16 +0200

    Add doc comment

--- frontend/src/utils/animation.ts
+++ frontend/src/utils/animation.ts
@@ -1,3 +1,4 @@
+// We should respect prefers-reduced-motion for those that turned off animations
 const showAnimations =
   window?.matchMedia(`(prefers-reduced-motion: reduce)`).matches ?? false;
 
