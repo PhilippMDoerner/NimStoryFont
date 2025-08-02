90b570cd282648fcf49551757920302c8ab8e48b
PhilippMDoerner <philippmdoerner@web.de>
2025-08-02 20:13:22 +0200

    Make animations accessible

--- frontend/src/utils/animation.ts
+++ frontend/src/utils/animation.ts
@@ -1,8 +1,18 @@
+import { environment } from 'src/environments/environment';
+
 // We should respect prefers-reduced-motion for those that turned off animations
 const showAnimations =
-  window?.matchMedia(`(prefers-reduced-motion: reduce)`).matches ?? false;
+  window?.matchMedia(`(prefers-reduced-motion: no-preference)`).matches ??
+  false;
+
+if (!showAnimations && environment.kind === 'DEVELOPMENT') {
+  console.warn('Animations disabled');
+}
 
 export function withViewTransition(cb: () => void): void {
-  if (!showAnimations) return;
-  document?.startViewTransition(() => cb());
+  if (!showAnimations) {
+    cb();
+  } else {
+    document?.startViewTransition(() => cb());
+  }
 }
