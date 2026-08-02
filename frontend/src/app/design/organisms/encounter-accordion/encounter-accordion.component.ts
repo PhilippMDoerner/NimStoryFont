a30e806098c7d79a833bd36bb197696340529ea0
PhilippMDoerner <philippmdoerner@web.de>
2026-08-02 17:14:36 +0200

    Add encounter pagination

--- frontend/src/app/design/organisms/encounter-accordion/encounter-accordion.component.ts
+++ frontend/src/app/design/organisms/encounter-accordion/encounter-accordion.component.ts
@@ -1,10 +1,13 @@
+import { NgTemplateOutlet } from '@angular/common';
 import {
   ChangeDetectionStrategy,
   Component,
+  computed,
   input,
   output,
+  signal,
 } from '@angular/core';
-import { NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbAccordionModule, NgbPagination } from '@ng-bootstrap/ng-bootstrap';
 import { HeadingDirective } from 'src/app/_directives/heading.directive';
 import { CharacterEncounter } from 'src/app/_models/character';
 import { Encounter, EncounterConnection } from 'src/app/_models/encounter';
@@ -16,16 +19,31 @@ import { EncounterComponent } from '../encounter/encounter.component';
   templateUrl: './encounter-accordion.component.html',
   styleUrls: ['./encounter-accordion.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [NgbAccordionModule, EncounterComponent, HeadingDirective],
+  imports: [
+    NgbAccordionModule,
+    EncounterComponent,
+    HeadingDirective,
+    NgbPagination,
+    NgTemplateOutlet,
+  ],
 })
 export class EncounterAccordionComponent {
-  encounters = input.required<(Encounter | CharacterEncounter)[]>();
-  campaignLocations = input.required<OverviewItem[]>();
-  campaignCharacters = input.required<OverviewItem[]>();
-  serverModel = input<Encounter>();
-  canUpdate = input(false);
-  canCreate = input(false);
-  canDelete = input(false);
+  readonly encounters = input.required<(Encounter | CharacterEncounter)[]>();
+  readonly campaignLocations = input.required<OverviewItem[]>();
+  readonly campaignCharacters = input.required<OverviewItem[]>();
+  readonly serverModel = input<Encounter>();
+  readonly canUpdate = input(false);
+  readonly canCreate = input(false);
+  readonly canDelete = input(false);
+
+  protected readonly page = signal(1);
+  protected readonly PAGE_SIZE = 5;
+  protected readonly displayedEncounters = computed(() => {
+    const pageIndex = this.page() - 1;
+    const startIndex = pageIndex * this.PAGE_SIZE;
+    const endIndex = startIndex + this.PAGE_SIZE;
+    return this.encounters().slice(startIndex, endIndex);
+  });
 
   readonly connectionDelete = output<EncounterConnection>();
   readonly connectionCreate = output<EncounterConnection>();
