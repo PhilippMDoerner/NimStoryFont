d39ac2f220097cf27da2dabf6e43076625135d2f
PhilippMDoerner <philippmdoerner@web.de>
2026-08-09 20:50:56 +0200

    Expand functionality

--- frontend/src/app/design/molecules/quill-editor/quill-editor.component.ts
+++ frontend/src/app/design/molecules/quill-editor/quill-editor.component.ts
@@ -5,12 +5,16 @@ import {
   effect,
   ElementRef,
   input,
+  model,
   output,
   viewChild,
 } from '@angular/core';
 import Quill, { QuillOptions } from 'quill';
+import E from 'quill/core/emitter';
 import { HOTKEY_IGNORE_ATTR } from 'src/app/_models/hotkey';
 import { QUILL_SETTINGS } from 'src/app/app.constants';
+
+type QuillEvent = (typeof E)['events'][keyof (typeof E)['events']];
 
 @Component({
   selector: 'app-quill-editor',
@@ -20,14 +24,19 @@ import { QUILL_SETTINGS } from 'src/app/app.constants';
   changeDetection: ChangeDetectionStrategy.OnPush,
   host: {
     [HOTKEY_IGNORE_ATTR]: '',
+    '(focusin)': 'onFocus.emit()',
+    '(focusout)': 'onBlur.emit()',
   },
 })
 export class QuillEditorComponent {
   private readonly container = viewChild<ElementRef<HTMLElement>>('editor');
 
+  readonly value = model.required<string>();
   readonly readOnly = input<boolean>(false);
 
   public readonly input = output<string>();
+  public readonly onBlur = output<void>();
+  public readonly onFocus = output<void>();
 
   private quillConfig = computed<QuillOptions>(() => ({
     ...QUILL_SETTINGS,
@@ -42,6 +51,34 @@ export class QuillEditorComponent {
   });
 
   constructor() {
-    effect(() => this.quill());
+    this.setupQuillEventListeners();
+
+    effect(() => {
+      const q = this.quill();
+      if (!q) return;
+
+      q.clipboard.dangerouslyPasteHTML(this.value());
+    });
+  }
+
+  private setupQuillEventListeners() {
+    effect(() => {
+      const q = this.quill();
+      if (!q) return;
+
+      const eventListeners = {
+        'text-change': () => this.input.emit(q.root.innerHTML ?? ''),
+      } satisfies Partial<Record<QuillEvent, Function>>;
+
+      Object.entries(eventListeners).forEach(([eventName, callback]) =>
+        q.on(eventName, callback),
+      );
+
+      return () => {
+        Object.entries(eventListeners).forEach(([eventName, callback]) =>
+          q.off(eventName, callback),
+        );
+      };
+    });
   }
 }
