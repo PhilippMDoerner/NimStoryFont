581a0a3f33c4b7a2baf2057c171e5ae8f34aa1f3
PhilippMDoerner <philippmdoerner@web.de>
2026-09-11 19:26:04 +0000

    Deploying to gh-pages from  @ 8519bd4479534abd58a63fd28256f8b82d987906 🚀

--- app/design/organisms/formly-editor-field/formly-editor-field.component.ts
+++ app/design/organisms/formly-editor-field/formly-editor-field.component.ts
@@ -1,12 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FieldType, FieldTypeConfig, FormlyModule } from '@ngx-formly/core';
+import { QuillEditorComponent } from '../../molecules/quill-editor/quill-editor.component';
 
 @Component({
   selector: 'app-formly-editor-field',
   templateUrl: './formly-editor-field.component.html',
   styleUrls: ['./formly-editor-field.component.scss'],
-  imports: [ReactiveFormsModule, FormlyModule],
+  imports: [ReactiveFormsModule, FormlyModule, QuillEditorComponent],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FormlyEditorFieldComponent extends FieldType<FieldTypeConfig> {}
